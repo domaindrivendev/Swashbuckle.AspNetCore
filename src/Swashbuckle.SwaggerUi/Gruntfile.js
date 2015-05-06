408846f6bd33cae34f9a9b9ccc4f52b2fb718fb3
domaindrivendev <domaindrivendev@gmail.com>
2015-05-05 22:05:45 -0700

    Update grunt to clean SwaggerUi. Keep index.html under source control

--- src/Swashbuckle.SwaggerUi/Gruntfile.js
+++ src/Swashbuckle.SwaggerUi/Gruntfile.js
@@ -4,21 +4,24 @@ Click here to learn more. http://go.microsoft.com/fwlink/?LinkID=513275&clcid=0x
 */
 module.exports = function (grunt) {
   grunt.initConfig({
-      pkg: grunt.file.readJSON('package.json'),
-      
-      bowercopy: {
-          options: {
-              runBower: true
-          },
-          libs: {
-              files: {
-                  "SwaggerUi": "swagger-ui/dist"
-              }
-          }
+    pkg: grunt.file.readJSON('package.json'),
+
+    clean: ['SwaggerUi'],
+    
+    bowercopy: {
+      options: {
+        runBower: true
+      },
+      libs: {
+        files: {
+          "SwaggerUi": "swagger-ui/dist"
+        }
       }
+    }
   });
 
+  grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-bowercopy');
 
-  grunt.registerTask('default', ['bowercopy:libs']);
+  grunt.registerTask('default', [ 'clean', 'bowercopy:libs' ]);
 };
