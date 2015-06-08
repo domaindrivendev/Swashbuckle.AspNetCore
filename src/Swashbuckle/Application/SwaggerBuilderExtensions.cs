f16db83862e2feb35d07b447642d560c18626dad
domaindrivendev <domaindrivendev@gmail.com>
2015-06-07 22:16:40 -0700

    UseFileServer middleware to serve swagger-ui

---
+++ src/Swashbuckle/Application/SwaggerBuilderExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.Framework.DependencyInjection;
+using Microsoft.AspNet.Routing;
+using Swashbuckle.Swagger;
+using Swashbuckle.Application;
+
+namespace Microsoft.AspNet.Builder
+{
+    public static class SwaggerBuilderExtensions
+    {
+        public static IApplicationBuilder UseSwagger(
+            this IApplicationBuilder app,
+            string routeTemplate = "swagger/{apiVersion}/swagger.json")
+        {
+            var routeBuilder = new RouteBuilder
+            {
+                DefaultHandler = CreateSwaggerDocsHandler(app.ApplicationServices),
+                ServiceProvider = app.ApplicationServices
+            };
+
+            routeBuilder.MapRoute("swagger_docs", routeTemplate);
+
+            return app.UseRouter(routeBuilder.Build());
+        }
+
+        private static IRouter CreateSwaggerDocsHandler(IServiceProvider serviceProvider)
+        {
+            var rootUrlResolver = serviceProvider.GetService<IRootUrlResolver>() ?? new DefaultRootUrlResolver();
+            var swaggerProvider = serviceProvider.GetRequiredService<ISwaggerProvider>();
+            return new SwaggerDocsHandler(rootUrlResolver, swaggerProvider);
+        }
+    }
+}
---
+++ src/Swashbuckle/Application/SwaggerUiBuilderExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System.Reflection;
+using Microsoft.AspNet.StaticFiles;
+using Microsoft.AspNet.FileProviders;
+
+namespace Microsoft.AspNet.Builder
+{
+    public static class SwaggerUiBuilderExtensions
+    {
+        public static void UseSwaggerUi(
+            this IApplicationBuilder app,
+            string requestPath = "/swagger/ui")
+        {
+            var options = new FileServerOptions();
+
+            options.RequestPath = requestPath;
+            options.DefaultFilesOptions.DefaultFileNames.Clear();
+            options.DefaultFilesOptions.DefaultFileNames.Add("index.html");
+
+            options.StaticFileOptions.ContentTypeProvider = new FileExtensionContentTypeProvider();
+
+            options.FileProvider = new EmbeddedFileProvider(
+                typeof(SwaggerUiBuilderExtensions).GetTypeInfo().Assembly,
+                "Swashbuckle.bower_components.swagger_ui.dist");
+
+            app.UseFileServer(options);
+        }
+    }
+}
