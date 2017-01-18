bbaade90b7c741e680b3fb42d8cfbe027c4263bc
mdschweda <marcus.schweda@posteo.de>
2017-01-18 17:55:43 +0100

    [SwaggerSchemaFilter] on structs

--- test/Swashbuckle.AspNetCore.SwaggerGen.Test/TestFixtures/Types/SwaggerAnnotatedType.cs
+++ test/Swashbuckle.AspNetCore.SwaggerGen.Test/TestFixtures/Types/SwaggerAnnotatedType.cs
@@ -1,7 +1,13 @@
 ﻿namespace Swashbuckle.AspNetCore.SwaggerGen.Test
 {
     [SwaggerSchemaFilter(typeof(VendorExtensionsSchemaFilter))]
-    public class SwaggerAnnotatedType
+    public class SwaggerAnnotatedClass
+    {
+        public string Property { get; set; }
+    }
+
+    [SwaggerSchemaFilter(typeof(VendorExtensionsSchemaFilter))]
+    public class SwaggerAnnotatedStruct
     {
         public string Property { get; set; }
     }
