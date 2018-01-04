098ef405882799877110e0b02d6ee7d8af59af37
domaindrivendev <domaindrivendev@gmail.com>
2018-01-03 16:29:12 -0800

    Incorporate newer patterns for embedded UI

---
+++ src/Swashbuckle.AspNetCore.SwaggerUI3/SwaggerUIBuilderExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Reflection;
+using Microsoft.Extensions.FileProviders;
+using Swashbuckle.AspNetCore.SwaggerUI3;
+
+namespace Microsoft.AspNetCore.Builder
+{
+    public static class SwaggerUIBuilderExtensions
+    {
+        private const string EmbeddedFilesNamespace = "Swashbuckle.AspNetCore.SwaggerUI3.bower_components.swagger_ui.dist";
+
+        public static IApplicationBuilder UseSwaggerUI3(
+            this IApplicationBuilder app,
+            Action<SwaggerUIOptions> setupAction)
+        {
+            var options = new SwaggerUIOptions();
+            setupAction?.Invoke(options);
+
+            app.UseMiddleware<SwaggerUIIndexMiddleware>(options);
+            app.UseFileServer(new FileServerOptions
+            {
+                RequestPath = $"/{options.RoutePrefix}",
+                FileProvider = new EmbeddedFileProvider(typeof(SwaggerUIBuilderExtensions).GetTypeInfo().Assembly, EmbeddedFilesNamespace),
+                EnableDefaultFiles = true //serve index.html at /{ options.RoutePrefix }/
+            });
+
+            return app;
+        }
+    }
+}
---
+++ src/Swashbuckle.AspNetCore.SwaggerUI3/SwaggerUIIndexMiddleware.cs
@@ -0,0 +1,65 @@
+﻿using System.Threading.Tasks;
+using System.IO;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+
+namespace Swashbuckle.AspNetCore.SwaggerUI3
+{
+    public class SwaggerUIIndexMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly SwaggerUIOptions _options;
+
+        public SwaggerUIIndexMiddleware(RequestDelegate next, SwaggerUIOptions options)
+        {
+            _next = next;
+            _options = options;
+        }
+
+        public async Task Invoke(HttpContext httpContext)
+        {
+            if (!RequestingSwaggerUIIndex(httpContext.Request))
+            {
+                await _next(httpContext);
+                return;
+            }
+
+            RespondWithIndexHtml(httpContext.Response);
+        }
+
+        private bool RequestingSwaggerUIIndex(HttpRequest request)
+        {
+            return (request.Method == "GET" && request.Path == $"/{_options.RoutePrefix}/");
+        }
+
+        private async void RespondWithIndexHtml(HttpResponse response)
+        {
+            response.StatusCode = 200;
+            response.ContentType = "text/html";
+
+            using (var stream = _options.IndexStream())
+            {
+                // Inject parameters before writing to response
+                var htmlBuilder = new StringBuilder(new StreamReader(stream).ReadToEnd());
+                foreach (var entry in GetIndexParameters())
+                {
+                    htmlBuilder.Replace(entry.Key, entry.Value);
+                }
+
+                await response.WriteAsync(htmlBuilder.ToString(), Encoding.UTF8);
+            }
+        }
+
+        private IDictionary<string, string> GetIndexParameters()
+        {
+            return new Dictionary<string, string>()
+            {
+                { "%(DocumentTitle)", _options.DocumentTitle },
+                { "%(HeadContent)", _options.HeadContent },
+                { "%(ConfigObject)", JsonConvert.SerializeObject(_options.ConfigObject) }
+            };
+        }
+    }
+}
