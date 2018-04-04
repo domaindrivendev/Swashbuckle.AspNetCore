1853eb7872125914e0131010e4c6e8444bd555b2
ErazerBrecht <erazerbrecht@gmail.com>
2018-04-04 11:30:54 +0200

    Fixed semicolons (eslint) + version release

--- src/Swashbuckle.AspNetCore.SwaggerUI/js/edge-fix.js
+++ src/Swashbuckle.AspNetCore.SwaggerUI/js/edge-fix.js
@@ -1,4 +1,4 @@
 ﻿if (window.navigator.userAgent.indexOf("Edge") > -1) {
-    console.log("Removing native Edge fetch in favor of swagger-ui's polyfill")
+    console.log("Removing native Edge fetch in favor of swagger-ui's polyfill");
     window.fetch = undefined;
 }
