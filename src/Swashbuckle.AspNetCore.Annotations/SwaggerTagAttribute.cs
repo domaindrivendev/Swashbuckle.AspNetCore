e0f77ec5416b6468273577ab94a8b2b374d4b5dd
Esben Bach <esben@ofn.dk>
2018-04-20 21:32:42 +0200

    Added som xmldoc comments to the tag attribute, and made the properties readonly.

--- src/Swashbuckle.AspNetCore.Annotations/SwaggerTagAttribute.cs
+++ src/Swashbuckle.AspNetCore.Annotations/SwaggerTagAttribute.cs
@@ -3,27 +3,46 @@ using System;
 
 namespace Swashbuckle.AspNetCore.Annotations
 {
+    /// <summary>
+    /// Defines document tags and related descriptions
+    /// </summary>
+    /// <remarks>
+    /// Tags defined on a controller will be added to the main swagger object.
+    /// </remarks>
     [AttributeUsage(AttributeTargets.Class, AllowMultiple = true)]
     public class SwaggerTagAttribute : Attribute
     {
-        public SwaggerTagAttribute(string name = null, string description = null, string externalDocUrls = null)
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SwaggerTagAttribute"/> class.
+        /// </summary>
+        /// <param name="name">The name of the tag, if an empty or whitespace name is given it wont be included</param>
+        /// <param name="description">An optional description for the tag</param>
+        /// <param name="externalDocUrl">An optional external document url</param>
+        public SwaggerTagAttribute(string name, string description = null, string externalDocUrl = null)
         {
             this.Name = name;
-
             this.Description = description;
-
             this.Tag = new Tag { Name = this.Name, Description = this.Description };
-            if (externalDocUrls != null)
-            {
-                this.Tag.ExternalDocs = new ExternalDocs { Url = externalDocUrls };
-            }
 
+            if (externalDocUrl != null)
+            {
+                this.Tag.ExternalDocs = new ExternalDocs { Url = externalDocUrl };
+            }
         }
 
-        public string Name { get; set; }
+        /// <summary>
+        /// Gets the name of the Tag
+        /// </summary>
+        public string Name { get; }
 
-        public string Description { get; set; }
+        /// <summary>
+        /// Gets the Description description for the tag
+        /// </summary>
+        public string Description { get;  }
 
+        /// <summary>
+        /// Gets the Tag that is going to be added to the Document model.
+        /// </summary>
         public Tag Tag { get; }
     }
 }
