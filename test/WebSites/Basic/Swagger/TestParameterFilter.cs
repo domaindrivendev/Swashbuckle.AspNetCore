85baf9788c5a4e3197fa95f30a5aaa17dab4d6c3
domaindrivendev <domaindrivendev@gmail.com>
2018-05-14 17:18:20 -0700

    Add parameter filters + clean up SwaggerGenerator

---
+++ test/WebSites/Basic/Swagger/TestParameterFilter.cs
@@ -0,0 +1,13 @@
+﻿using Swashbuckle.AspNetCore.SwaggerGen;
+using Swashbuckle.AspNetCore.Swagger;
+
+namespace Basic.Swagger
+{
+    public class TestParameterFilter : IParameterFilter
+    {
+        public void Apply(IParameter parameter, ParameterFilterContext context)
+        {
+            parameter.Extensions.Add("x-foobar", true);
+        }
+    }
+}
