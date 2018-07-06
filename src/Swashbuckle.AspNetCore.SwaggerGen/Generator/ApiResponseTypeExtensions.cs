754cc818bcea95a38338186e53da811aee2c9893
Pranav K <prkrishn@hotmail.com>
2018-07-06 14:00:46 -0700

    Add support for default response

---
+++ src/Swashbuckle.AspNetCore.SwaggerGen/Generator/ApiResponseTypeExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using System;
+using System.Reflection;
+
+namespace Swashbuckle.AspNetCore.SwaggerGen
+{
+    public static class ApiResponseTypeExtensions
+    {
+        internal static bool IsDefaultResponse(this ApiResponseType apiResponseType)
+        {
+            var propertyInfo = apiResponseType.GetType().GetProperty("IsDefaultResponse");
+            if (propertyInfo != null)
+            {
+                return (bool)propertyInfo.GetValue(apiResponseType);
+            }
+
+            // ApiExplorer < 2.1.0 does not support default response.
+            return false;
+        }
+    }
+}
