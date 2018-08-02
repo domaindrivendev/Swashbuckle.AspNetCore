1cd90fd72e14bd0f92de390a8469ab6e5f8f190b
Eduardo Cáceres <edcc@gmv.com>
2018-08-02 11:57:20 +0200

    Skip tests that use http://online.swagger.io/, since it seems to be down
    Those tests also fail in https://github.com/domaindrivendev/Swashbuckle.AspNetCore/tree/master

--- test/Swashbuckle.AspNetCore.IntegrationTests/SwaggerGenIntegrationTests.cs
+++ test/Swashbuckle.AspNetCore.IntegrationTests/SwaggerGenIntegrationTests.cs
@@ -21,7 +21,7 @@ namespace Swashbuckle.AspNetCore.IntegrationTests
             };
         }
 
-        [Theory]
+        [Theory(Skip = "http://online.swagger.io/ seems to be down")]
         [InlineData(typeof(Basic.Startup), "/swagger/v1/swagger.json")]
         [InlineData(typeof(CustomUIConfig.Startup), "/swagger/v1/swagger.json")]
         [InlineData(typeof(CustomUIIndex.Startup), "/swagger/v1/swagger.json")]
