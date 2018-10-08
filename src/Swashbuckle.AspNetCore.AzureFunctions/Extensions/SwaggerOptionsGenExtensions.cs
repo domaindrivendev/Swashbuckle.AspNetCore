31949b863e95a2bdce1faa90f99722cb4c312fbf
Ben Herila <ben@herila.net>
2018-10-07 21:07:41 -0700

    Missing import

--- src/Swashbuckle.AspNetCore.AzureFunctions/Extensions/SwaggerOptionsGenExtensions.cs
+++ src/Swashbuckle.AspNetCore.AzureFunctions/Extensions/SwaggerOptionsGenExtensions.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.IO;
 using System.Reflection;
+using Microsoft.Extensions.DependencyInjection;
 using Swashbuckle.AspNetCore.SwaggerGen;
 
 namespace Swashbuckle.AspNetCore.AzureFunctions.Extensions
