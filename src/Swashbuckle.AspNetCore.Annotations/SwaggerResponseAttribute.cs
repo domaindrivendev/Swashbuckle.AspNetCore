ea58d9cdc167a7a63b000ca25f1d285422bc206f
Ben Herila <ben@herila.net>
2018-11-14 09:19:55 -0800

    Don't inherit from ProducesResponseTypeAttribute

--- src/Swashbuckle.AspNetCore.Annotations/SwaggerResponseAttribute.cs
+++ src/Swashbuckle.AspNetCore.Annotations/SwaggerResponseAttribute.cs
@@ -7,17 +7,27 @@ namespace Swashbuckle.AspNetCore.Annotations
     /// Adds or enriches Response metadata for a given action method
     /// </summary>
     [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = true, Inherited = true)]
-    public class SwaggerResponseAttribute : ProducesResponseTypeAttribute
+    public class SwaggerResponseAttribute : Attribute
     {
-        public SwaggerResponseAttribute(int statusCode, string description = null, Type type = null)
-            : base(type ?? typeof(void), statusCode)
-        {
+        public SwaggerResponseAttribute(int statusCode, string description = null, Type type = null) {
+            StatusCode = statusCode;
             Description = description;
+            Type = type;
         }
+
+        /// <summary>
+        /// Status code of the response.
+        /// </summary>
+        public int StatusCode { get; }
 
         /// <summary>
         /// A short description of the response. GFM syntax can be used for rich text representation.
         /// </summary>
         public string Description { get; set; }
+
+        /// <summary>
+        /// Defines the type returned by the response.
+        /// </summary>
+        public Type Type { get; }
     }
 }
