b5cb3f9f6ddf674daa40748b3431f67519d433e5
domaindrivendev <domaindrivendev@gmail.com>
2019-02-21 12:25:44 +0000

    Support annotation-based approach to polymorphic schemas

---
+++ src/Swashbuckle.AspNetCore.Annotations/SwaggerSubTypeAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Swashbuckle.AspNetCore.Annotations
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct, AllowMultiple = true)]
+    public class SwaggerSubTypeAttribute : Attribute
+    {
+        public SwaggerSubTypeAttribute(Type subType)
+        {
+            SubType = subType;
+        }
+
+        public Type SubType { get; }
+    }
+}
