11f13ca3aba25bb0c1fc6fce1af65c0b3dd7452f
Oskar Gewalli <wallymathieu@users.noreply.github.com>
2020-03-19 22:34:32 +0100

    Changes after code review

--- src/Swashbuckle.AspNetCore.SwaggerGen/SchemaGenerator/MemberInfoExtensions.cs
+++ src/Swashbuckle.AspNetCore.SwaggerGen/SchemaGenerator/MemberInfoExtensions.cs
@@ -9,17 +9,15 @@ namespace Swashbuckle.AspNetCore.SwaggerGen
     {
         public static IEnumerable<object> GetInlineOrMetadataTypeAttributes(this MemberInfo memberInfo)
         {
-            if (memberInfo==null) throw new NullReferenceException(nameof(memberInfo));
-            var metadataTypeAttribute = memberInfo.DeclaringType.GetCustomAttributes(true)
+            var metadataTypeAttribute = memberInfo?.DeclaringType.GetCustomAttributes(true)
                 .OfType<ModelMetadataTypeAttribute>()
                 .FirstOrDefault();
-            if (memberInfo.Name==null) throw new NullReferenceException(nameof(memberInfo.Name));
 
-            var metadataTypeMemberInfo = metadataTypeAttribute?.MetadataType.GetMember(memberInfo.Name)
-                .FirstOrDefault();
+            var metadataTypeMemberInfo = metadataTypeAttribute?.MetadataType.GetMember(memberInfo?.Name)
+                ?.FirstOrDefault();
 
             return (metadataTypeMemberInfo == null)
-                ? memberInfo.GetCustomAttributes(true)
+                ? memberInfo?.GetCustomAttributes(true)
                 : metadataTypeMemberInfo.GetCustomAttributes(true);
         }
     }
