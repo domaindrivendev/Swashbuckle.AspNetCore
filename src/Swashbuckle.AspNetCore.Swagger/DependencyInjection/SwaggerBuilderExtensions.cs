6da4839c2b3feec7c1770d4ed140651600990bfa
Wuyafeng <yafeng.wu@outlook.com>
2020-04-14 13:55:24 +0800

    Resolve conversation: * Avoid duplicaion logic by using SwaggerMiddleware * Use singular form in message * Avoid `RouteTemplate` in endpoint routing

--- src/Swashbuckle.AspNetCore.Swagger/DependencyInjection/SwaggerBuilderExtensions.cs
+++ src/Swashbuckle.AspNetCore.Swagger/DependencyInjection/SwaggerBuilderExtensions.cs
@@ -39,14 +39,28 @@ namespace Microsoft.AspNetCore.Builder
             return app;
         }
 #if NETCOREAPP3_0
-        public static IEndpointRouteBuilder MapSwagger(this IEndpointRouteBuilder endpoints, string pattern)
+        public static IEndpointRouteBuilder MapSwagger(
+            this IEndpointRouteBuilder endpoints,
+            string pattern,
+            Action<SwaggerOptions> setupAction = null)
         {
             if (!RoutePatternFactory.Parse(pattern).Parameters.Any(x => x.Name == "documentName"))
             {
-                throw new ArgumentException("Pattern must contains '{documentName}' parameter", nameof(pattern));
+                throw new ArgumentException("Pattern must contain '{documentName}' parameter", nameof(pattern));
             }
 
-            endpoints.MapGet(pattern, InvokeAsync);
+            Action<SwaggerOptions> endpointSetupAction = options =>
+            {
+                setupAction?.Invoke(options);
+
+                options.RouteTemplate = pattern;
+            };
+
+            var pipeline = endpoints.CreateApplicationBuilder()
+                .UseSwagger(endpointSetupAction)
+                .Build();
+
+            endpoints.MapGet(pattern, pipeline);
 
             return endpoints;
         }
@@ -54,53 +68,6 @@ namespace Microsoft.AspNetCore.Builder
         public static IEndpointRouteBuilder MapSwagger(this IEndpointRouteBuilder endpoints)
         {
             return endpoints.MapSwagger("/swagger/{documentName}/swagger.json");
-        }
-
-        private async static Task InvokeAsync(HttpContext context)
-        {
-            var response = context.Response;
-            if (context.Request.RouteValues.TryGetValue("documentName", out var documentName))
-            {
-                var provider = context.RequestServices.GetRequiredService<ISwaggerProvider>();
-                var basePath = string.IsNullOrEmpty(context.Request.PathBase)
-                    ? null
-                    : context.Request.PathBase.ToString();
-
-                try
-                {
-                    var swagger = provider.GetSwagger(documentName.ToString(), null, basePath);
-
-                    var options = context.RequestServices.GetRequiredService<IOptions<SwaggerOptions>>().Value;
-
-                    foreach (var filter in options.PreSerializeFilters)
-                    {
-                        filter(swagger, context.Request);
-                    }
-
-                    response.StatusCode = 200;
-                    response.ContentType = "application/json;charset=utf-8";
-
-                    using (var textWriter = new StringWriter(CultureInfo.InvariantCulture))
-                    {
-                        var jsonWriter = new OpenApiJsonWriter(textWriter);
-                        if (options.SerializeAsV2) swagger.SerializeAsV2(jsonWriter);
-                        else
-                        {
-                            swagger.SerializeAsV3(jsonWriter);
-                        }
-
-                        await response.WriteAsync(textWriter.ToString(), new UTF8Encoding(false));
-                    }
-
-                    return;
-                }
-                catch (UnknownSwaggerDocument)
-                {
-
-                }
-            }
-
-            response.StatusCode = StatusCodes.Status404NotFound;
         }
 #endif
     }
