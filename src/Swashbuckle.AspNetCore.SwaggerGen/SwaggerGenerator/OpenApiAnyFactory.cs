8ff7b32838e46ca0f748bd3f443840fbb07fd45d
rmorris <richard.morris@gettyimages.com>
2020-05-13 14:35:30 +0100

    Update OpenApiAnyFactory to be consistent with ...ApiTesting.OpenApiSchemaExtensions

--- src/Swashbuckle.AspNetCore.SwaggerGen/SwaggerGenerator/OpenApiAnyFactory.cs
+++ src/Swashbuckle.AspNetCore.SwaggerGen/SwaggerGenerator/OpenApiAnyFactory.cs
@@ -10,26 +10,29 @@ namespace Swashbuckle.AspNetCore.SwaggerGen
         {
             if (value == null) return null;
 
-            if (schema.Type == "boolean" && TryCast(value, out bool boolValue))
-                return new OpenApiBoolean(boolValue);
-
-            else if (schema.Type == "integer" && schema.Format == "int32" && TryCast(value, out int intValue))
-                return new OpenApiInteger(intValue);
-
-            else if (schema.Type == "integer" && schema.Format == "int64" && TryCast(value, out long longValue))
+            if (schema.Type == "integer" && schema.Format == "int64" && TryCast(value, out long longValue))
                 return new OpenApiLong(longValue);
 
-            else if (schema.Type == "number" && schema.Format == "float" && TryCast(value, out float floatValue))
-                return new OpenApiFloat(floatValue);
+            else if (schema.Type == "integer" && TryCast(value, out int intValue))
+                return new OpenApiInteger(intValue);
 
             else if (schema.Type == "number" && schema.Format == "double" && TryCast(value, out double doubleValue))
                 return new OpenApiDouble(doubleValue);
 
-            else if (schema.Type == "string" && value.GetType().IsEnum)
-                return new OpenApiString(Enum.GetName(value.GetType(), value));
+            else if (schema.Type == "number" && TryCast(value, out float floatValue))
+                return new OpenApiFloat(floatValue);
+
+            if (schema.Type == "boolean" && TryCast(value, out bool boolValue))
+                return new OpenApiBoolean(boolValue);
+
+            else if (schema.Type == "string" && schema.Format == "date" && TryCast(value, out DateTime dateValue))
+                return new OpenApiDate(dateValue);
 
             else if (schema.Type == "string" && schema.Format == "date-time" && TryCast(value, out DateTime dateTimeValue))
                 return new OpenApiDate(dateTimeValue);
+
+            else if (schema.Type == "string" && value.GetType().IsEnum)
+                return new OpenApiString(Enum.GetName(value.GetType(), value));
 
             else if (schema.Type == "string")
                 return new OpenApiString(value.ToString());
