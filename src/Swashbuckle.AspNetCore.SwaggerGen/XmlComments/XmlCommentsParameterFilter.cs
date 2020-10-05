12c4d25d29a97760aef7ac920867b38684ad5333
rmorris <richard.morris@gettyimages.com>
2020-10-05 15:59:46 +0100

    Only apply member XML tags if not bound to request parameter

--- src/Swashbuckle.AspNetCore.SwaggerGen/XmlComments/XmlCommentsParameterFilter.cs
+++ src/Swashbuckle.AspNetCore.SwaggerGen/XmlComments/XmlCommentsParameterFilter.cs
@@ -1,5 +1,9 @@
-﻿using System.Reflection;
+﻿using System;
+using System.ComponentModel;
+using System.Globalization;
+using System.Reflection;
 using System.Xml.XPath;
+using Microsoft.OpenApi.Any;
 using Microsoft.OpenApi.Models;
 
 namespace Swashbuckle.AspNetCore.SwaggerGen
@@ -28,10 +32,20 @@ namespace Swashbuckle.AspNetCore.SwaggerGen
         private void ApplyPropertyTags(OpenApiParameter parameter, PropertyInfo propertyInfo)
         {
             var propertyMemberName = XmlCommentsNodeNameHelper.GetMemberNameForFieldOrProperty(propertyInfo);
-            var propertySummaryNode = _xmlNavigator.SelectSingleNode($"/doc/members/member[@name='{propertyMemberName}']/summary");
+            var propertyNode = _xmlNavigator.SelectSingleNode($"/doc/members/member[@name='{propertyMemberName}']");
 
-            if (propertySummaryNode != null)
-                parameter.Description = XmlCommentsTextHelper.Humanize(propertySummaryNode.InnerXml);
+            if (propertyNode == null) return;
+
+            var summaryNode = propertyNode.SelectSingleNode("summary");
+            if (summaryNode != null)
+                parameter.Description = XmlCommentsTextHelper.Humanize(summaryNode.InnerXml);
+
+            var exampleNode = propertyNode.SelectSingleNode("example");
+            if (exampleNode != null)
+            {
+                var exampleString = XmlCommentsTextHelper.Humanize(exampleNode.InnerXml);
+                parameter.Example = ConvertToOpenApiType(propertyInfo.PropertyType, parameter.Schema, exampleString);
+            }
         }
 
         private void ApplyParamTags(OpenApiParameter parameter, ParameterInfo parameterInfo)
@@ -52,7 +66,32 @@ namespace Swashbuckle.AspNetCore.SwaggerGen
             if (paramNode != null)
             {
                 parameter.Description = XmlCommentsTextHelper.Humanize(paramNode.InnerXml);
+
+                var example = paramNode.GetAttribute("example", "");
+                if (!string.IsNullOrEmpty(example))
+                {
+                    parameter.Example = ConvertToOpenApiType(parameterInfo.ParameterType, parameter.Schema, example);
+                }
             }
+        }
+
+        private static IOpenApiAny ConvertToOpenApiType(Type type, OpenApiSchema schema, string stringValue)
+        {
+            object typedValue;
+
+            try
+            {
+                typedValue = TypeDescriptor.GetConverter(type).ConvertFrom(
+                    context: null,
+                    culture: CultureInfo.InvariantCulture,
+                    stringValue);
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+
+            return OpenApiAnyFactory.CreateFor(schema, typedValue);
         }
     }
 }
