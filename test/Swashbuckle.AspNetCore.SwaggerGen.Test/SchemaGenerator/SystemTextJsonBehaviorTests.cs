b82756d004a2bbfff2ba4f6c53123d2892d0df33
navferty <navferty@ymail.com>
2020-11-03 16:41:59 +0300

    include base interfaces props in SchemaGenerator

---
+++ test/Swashbuckle.AspNetCore.SwaggerGen.Test/SchemaGenerator/SystemTextJsonBehaviorTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+using System.Text.Json;
+
+using Xunit;
+
+using Swashbuckle.AspNetCore.TestSupport;
+
+namespace Swashbuckle.AspNetCore.SwaggerGen.Test
+{
+    public class SystemTextJsonBehaviorTests
+    {
+        [Theory]
+        [InlineData(typeof(IBaseInterface), new[] { "BaseProperty" })]
+        [InlineData(typeof(ISubInterface1), new[] { "BaseProperty", "Property1" })]
+        [InlineData(typeof(ISubInterface2), new[] { "BaseProperty", "Property2" })]
+        [InlineData(typeof(IMultiSubInterface), new[] { "BaseProperty", "Property1", "Property2", "Property3" })]
+        public void GetDataContractForType_CreatedContracr_AllInterfaceMembersIncluded(Type type, string[] propertyNames)
+        {
+            var behavior = Subject();
+
+            var contract = behavior.GetDataContractForType(type);
+
+            var contractPropertiesNames = contract.ObjectProperties
+                .Select(x => x.Name)
+                .OrderBy(x => x)
+                .ToList();
+            Assert.Equal(propertyNames, contractPropertiesNames);
+        }
+
+        private SystemTextJsonBehavior Subject(Action<JsonSerializerOptions> configureSerializer = null)
+        {
+            var serializerOptions = new JsonSerializerOptions();
+            configureSerializer?.Invoke(serializerOptions);
+
+            return new SystemTextJsonBehavior(serializerOptions);
+        }
+    }
+}
