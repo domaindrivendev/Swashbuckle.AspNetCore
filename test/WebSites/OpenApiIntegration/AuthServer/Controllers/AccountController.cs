d396eca2b2046bca7327d0942307a2e4908b6478
Ihnat Klimchuk <12488585+IhnatKlimchuk@users.noreply.github.com>
2020-12-29 10:42:42 +0300

    Allow OpenAPI support

---
+++ test/WebSites/OpenApiIntegration/AuthServer/Controllers/AccountController.cs
@@ -0,0 +1,54 @@
+﻿using IdentityServer4;
+using IdentityServer4.Test;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace OpenApiIntegration.AuthServer.Controllers
+{
+    [ApiExplorerSettings(IgnoreApi = true)]
+    [Route("account")]
+    public class AccountController : Controller
+    {
+        private readonly TestUserStore _userStore;
+
+        public AccountController()
+        {
+            _userStore = new TestUserStore(Config.TestUsers());
+        }
+
+        [HttpGet("login")]
+        public IActionResult Login(string returnUrl)
+        {
+            var viewModel = new LoginViewModel { Username = "joebloggs", Password = "pass123", ReturnUrl = returnUrl };
+
+            return View("/AuthServer/Views/Login.cshtml", viewModel);
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromForm]LoginViewModel viewModel)
+        {
+            if (!_userStore.ValidateCredentials(viewModel.Username, viewModel.Password))
+            {
+                ModelState.AddModelError("", "Invalid username or password");
+                viewModel.Password = string.Empty;
+                return View("/AuthServer/Views/Login.cshtml", viewModel);
+            }
+
+            // Use an IdentityServer-compatible ClaimsPrincipal
+            var identityServerUser = new IdentityServerUser(viewModel.Username);
+            identityServerUser.DisplayName = viewModel.Username;
+            await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, identityServerUser.CreatePrincipal());
+
+            return Redirect(viewModel.ReturnUrl);
+        }
+    }
+
+    public class LoginViewModel
+    {
+        public string ReturnUrl { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
