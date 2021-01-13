4311a4d8360e12df75fd950165128d739e77dafa
rmorris <richard.morris@gettyimages.com>
2021-01-13 17:56:31 +0000

    Some code cleanup to middleware builder extensions

--- src/Swashbuckle.AspNetCore.SwaggerUI/SwaggerUIBuilderExtensions.cs
+++ src/Swashbuckle.AspNetCore.SwaggerUI/SwaggerUIBuilderExtensions.cs
@@ -8,34 +8,29 @@ namespace Microsoft.AspNetCore.Builder
     public static class SwaggerUIBuilderExtensions
     {
         /// <summary>
-        /// Register the SwaggerUI middleware. Use this overload for default settings or if you want to inject SwaggerUiOptions options via DI
-        /// </summary>
-        public static IApplicationBuilder UseSwaggerUI(this IApplicationBuilder app)
-        {
-            return app.UseSwaggerUI(app.ApplicationServices.GetRequiredService<IOptions<SwaggerUIOptions>>().Value);
-        }
-
-        /// <summary>
         /// Register the SwaggerUI middleware with provided options
         /// </summary>
         public static IApplicationBuilder UseSwaggerUI(this IApplicationBuilder app, SwaggerUIOptions options)
         {
+            return app.UseMiddleware<SwaggerUIMiddleware>(options);
+        }
+
+        /// <summary>
+        /// Register the SwaggerUI middleware with or without additional config action
+        /// </summary>
+        public static IApplicationBuilder UseSwaggerUI(
+            this IApplicationBuilder app,
+            Action<SwaggerUIOptions> setupAction = null)
+        {
+            var options = app.ApplicationServices.GetRequiredService<IOptions<SwaggerUIOptions>>().Value;
+
+            setupAction?.Invoke(options);
+
             // To simplify the common case, use a default that will work with the SwaggerMiddleware defaults
             if (options.ConfigObject.Urls == null)
             {
                 options.ConfigObject.Urls = new[] { new UrlDescriptor { Name = "V1 Docs", Url = "/swagger/v1/swagger.json" } };
             }
-
-            return app.UseMiddleware<SwaggerUIMiddleware>(options);
-        }
-
-        /// <summary>
-        /// Register the SwaggerUI middleware with provided options
-        /// </summary>
-        public static IApplicationBuilder UseSwaggerUI(this IApplicationBuilder app, Action<SwaggerUIOptions> setupAction)
-        {
-            var options = app.ApplicationServices.GetRequiredService<IOptions<SwaggerUIOptions>>().Value;
-            setupAction(options);
 
             return app.UseSwaggerUI(options);
         }
