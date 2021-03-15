9387842c0f345465f687997a42dda0058093eef5
kiril.karagyozov <kiril.karagyozov@reconart.com>
2021-03-15 11:20:44 +0200

    Added support for array type, along with honoring constructor conventions and bug fixes

--- src/Swashbuckle.AspNetCore.Annotations/SwaggerMultipartFormDataAttribute.cs
+++ src/Swashbuckle.AspNetCore.Annotations/SwaggerMultipartFormDataAttribute.cs
@@ -15,23 +15,16 @@ namespace Swashbuckle.AspNetCore.Annotations
     [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = true, Inherited = true)]
     public class SwaggerMultiPartFormDataAttribute : Attribute
     {
-        public SwaggerMultiPartFormDataAttribute() : this("file", false)
-        {
-        }
 
-        public SwaggerMultiPartFormDataAttribute(string name) : this(name, false)
-        {
-        }
-
-        public SwaggerMultiPartFormDataAttribute(bool isRequired) : this("file", isRequired)
-        {
-        }
-
-        public SwaggerMultiPartFormDataAttribute(string name, bool required)
+        public SwaggerMultiPartFormDataAttribute(string name = null, bool required = false, string type = null, string collectionType = null, string format = null)
         {
             Name = name;
             Required = required;
+            Type = type;
+            CollectionType= collectionType;
+            Format = format;
         }
+        private string _collectionType;
 
         /// <summary>
         /// Name of the boundary.
@@ -46,7 +39,32 @@ namespace Swashbuckle.AspNetCore.Annotations
         /// <summary>
         /// Data type of the boundary as defined in OpenAPI specifications: https://swagger.io/docs/specification/data-models/data-types/
         /// </summary>
+        /// <remarks>
+        /// Supports "array" type for defining a collection of multiple boundaries.
+        /// </remarks>
         public string Type { get; set; }
+
+        /// <summary>
+        /// Data type of the boundary in a collection as defined in OpenAPI specifications: https://swagger.io/docs/specification/data-models/data-types/
+        /// </summary>
+        /// <remarks>
+        /// Used when <seealso cref="Type"/> is "array".
+        /// </remarks>
+        /// <exception cref="InvalidOperationException">Thrown when <seealso cref="CollectionType"/> is set to "array".</exception>
+        public string CollectionType
+        {
+            get => _collectionType;
+
+            set
+            {
+                if (string.CompareOrdinal(value, "array") == 0)
+                {
+                    throw new InvalidOperationException($"{nameof(CollectionType)} cannot be an array.");
+                }
+
+                _collectionType = value;
+            }
+        }
 
         /// <summary>
         /// Format of boundary's data type as defined in OpenAPI specifications: https://swagger.io/docs/specification/data-models/data-types/
