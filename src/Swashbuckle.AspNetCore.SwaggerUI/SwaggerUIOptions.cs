ce304488e9272c4515783f92583e6acd64818934
scharnyw <35246522+scharnyw@users.noreply.github.com>
2021-10-25 21:21:01 +0800

    Fix XML comments on Swagger UI interceptors

--- src/Swashbuckle.AspNetCore.SwaggerUI/SwaggerUIOptions.cs
+++ src/Swashbuckle.AspNetCore.SwaggerUI/SwaggerUIOptions.cs
@@ -234,7 +234,7 @@ namespace Swashbuckle.AspNetCore.SwaggerUI
         /// MUST be a valid Javascript function.
         /// Function to intercept remote definition, "Try it out", and OAuth 2.0 requests.
         /// Accepts one argument requestInterceptor(request) and must return the modified request, or a Promise that resolves to the modified request.
-        /// Ex: "req => { req.headers['MyCustomHeader'] = 'CustomValue'; return req; }"
+        /// Ex: "function (req) { req.headers['MyCustomHeader'] = 'CustomValue'; return req; }"
         /// </summary>
         public string RequestInterceptorFunction { get; set; }
 
@@ -242,7 +242,7 @@ namespace Swashbuckle.AspNetCore.SwaggerUI
         /// MUST be a valid Javascript function.
         /// Function to intercept remote definition, "Try it out", and OAuth 2.0 responses.
         /// Accepts one argument responseInterceptor(response) and must return the modified response, or a Promise that resolves to the modified response.
-        /// Ex: "res => { console.log(res); return res; }"
+        /// Ex: "function (res) { console.log(res); return res; }"
         /// </summary>
         public string ResponseInterceptorFunction { get; set; }
     }
