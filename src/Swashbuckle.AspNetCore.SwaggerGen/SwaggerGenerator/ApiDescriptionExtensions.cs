a84e87452cd8825c3f8c972bb46e958d0d7de57d
Ralf Beckers <rbeckers@microsoft.com>
2021-10-25 10:56:46 +0200

    Add support for using colons in path
    Fi issue where colons outside of parameters where processed instead of being copied verbatim.

--- src/Swashbuckle.AspNetCore.SwaggerGen/SwaggerGenerator/ApiDescriptionExtensions.cs
+++ src/Swashbuckle.AspNetCore.SwaggerGen/SwaggerGenerator/ApiDescriptionExtensions.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Reflection;
 using Microsoft.AspNetCore.Mvc.ApiExplorer;
 using Microsoft.AspNetCore.Mvc.Controllers;
+using System.Text;
 
 namespace Swashbuckle.AspNetCore.SwaggerGen
 {
@@ -51,28 +52,57 @@ namespace Swashbuckle.AspNetCore.SwaggerGen
         internal static string RelativePathSansParameterConstraints(this ApiDescription apiDescription)
         {
             var routeTemplate = apiDescription.RelativePath;
-            
-            // We want to filter out qualifiers that indicate a constract (":")
-            // a default value ("=") or an optional parameter ("?")
-            while (routeTemplate.IndexOfAny(new[] { ':', '=', '?' }) != -1)
-            {
-                var startIndex = routeTemplate.IndexOfAny(new[] { ':', '=', '?' }) ;
-                var tokenStart = startIndex + 1;
-                // Only find a single instance of a '}' after our start
-                // character to avoid capturing escaped curly braces
-                // in a regular expression constraint
-                findEndBrace:
-                    var endIndex = routeTemplate.IndexOf('}', tokenStart);
-                    if (endIndex < routeTemplate.Length - 1 && routeTemplate[endIndex + 1] == '}')
-                    {
-                        tokenStart = endIndex + 2;
-                        goto findEndBrace;
-                    }
 
-                routeTemplate = routeTemplate.Remove(startIndex, endIndex - startIndex);
+            // We want to filter out qualifiers that indicate a constraint (":") a default value ("=") or an optional parameter ("?").
+            // The filtering should be only done inside of curlies. Outside of them they are part of the normal defined route.
+
+            var result = new StringBuilder();
+            var index = 0;
+            int nextOpenCurly;
+            while ((nextOpenCurly = routeTemplate.IndexOf('{', index)) != -1)
+            {
+                // append everything before the '{' to the result
+                result.Append(routeTemplate.Substring(index, nextOpenCurly - index));
+                index = nextOpenCurly;
+
+                var nextClosingCurly = routeTemplate.IndexOf('}', index);
+                var contentFromCurly = routeTemplate.Substring(index, nextClosingCurly - index + 1);
+
+                int separatorIndex;
+                if ((separatorIndex = contentFromCurly.IndexOf('=')) != -1)
+                {
+                    // for a default value, take everything after the '='
+                    // e.g. "{controller=Home}/{action=Index}" => "{Home}/{Index}"
+                    result.Append('{');
+                    result.Append(contentFromCurly.Substring(separatorIndex + 1));
+                }
+                else if ((separatorIndex = contentFromCurly.IndexOf(':')) != -1)
+                {
+                    // for a contraint, take everything before the constraint
+                    // e.g. "collection/{id:int}" => "collection/{id}",
+                    result.Append(contentFromCurly.Substring(0, separatorIndex));
+                    result.Append('}');
+                }
+                else if ((separatorIndex = contentFromCurly.IndexOf('?')) != -1)
+                {
+                    // for an option parameter, just leave out the '?'
+                    // e.g. "action/{id?}" => "action/{id}"
+                    result.Append(contentFromCurly.Substring(0, separatorIndex));
+                    result.Append('}');
+                }
+                else
+                {
+                    // there was no separator found, append the whole content
+                    result.Append(contentFromCurly);
+                }
+
+                index = nextClosingCurly + 1;
             }
-            
-            return routeTemplate;
+
+            // use everything after the last curlies
+            result.Append(routeTemplate.Substring(index));
+
+            return result.ToString();
         }
     }
 }
