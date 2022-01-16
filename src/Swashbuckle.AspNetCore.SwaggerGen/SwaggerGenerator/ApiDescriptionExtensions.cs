5344f6cd17d5ee93fa311a53be2b422d6f21e9df
Chris/0 <cosborn@cimpress.com>
2022-01-16 09:46:52 -0500

    Switch Parameter Constraint Stripping to Use Template Parsing
    Rather than recreate what the template parser is doing, let's use it
    instead.

--- src/Swashbuckle.AspNetCore.SwaggerGen/SwaggerGenerator/ApiDescriptionExtensions.cs
+++ src/Swashbuckle.AspNetCore.SwaggerGen/SwaggerGenerator/ApiDescriptionExtensions.cs
@@ -2,16 +2,14 @@
 using System.Linq;
 using System.Collections.Generic;
 using System.Reflection;
-using System.Text;
 using Microsoft.AspNetCore.Mvc.ApiExplorer;
 using Microsoft.AspNetCore.Mvc.Controllers;
+using Microsoft.AspNetCore.Routing.Template;
 
 namespace Swashbuckle.AspNetCore.SwaggerGen
 {
     public static class ApiDescriptionExtensions
     {
-        private static readonly char[] Disallowed = { ':', '=', '?' };
-
         public static bool TryGetMethodInfo(this ApiDescription apiDescription, out MethodInfo methodInfo)
         {
             if (apiDescription.ActionDescriptor is ControllerActionDescriptor controllerActionDescriptor)
@@ -53,56 +51,11 @@ namespace Swashbuckle.AspNetCore.SwaggerGen
 
         internal static string RelativePathSansParameterConstraints(this ApiDescription apiDescription)
         {
-            var routeTemplate = apiDescription.RelativePath;
-
-            // We want to filter out qualifiers that indicate a constraint (":")
-            // a default value ("=") or an optional parameter ("?")
-            if (routeTemplate.IndexOfAny(Disallowed) == -1)
-            {
-                // Quick-exit if there's nothing to remove.
-                return routeTemplate;
-            }
-
-            var modifiedTemplate = new StringBuilder(routeTemplate.Length);
-            var index = 0;
-            var isBetweenCurlyBraces = false;
-            while (index < routeTemplate.Length)
-            {
-                var current = routeTemplate[index];
-
-                if (current == '{')
-                {
-                    isBetweenCurlyBraces = true;
-                    goto next;
-                }
-
-                if (current == '}')
-                {
-                    isBetweenCurlyBraces = false;
-                    goto next;
-                }
-
-                if (isBetweenCurlyBraces && Disallowed.Contains(current))
-                {
-                    // Only find a single instance of a '}' after our start
-                    // character to avoid capturing escaped curly braces
-                    // in a regular expression constraint
-                findEndBrace:
-                    index = routeTemplate.IndexOf('}', index + 1);
-                    if (index < routeTemplate.Length - 1 && routeTemplate[index + 1] == '}')
-                    {
-                        index += 2;
-                        goto findEndBrace;
-                    }
-                    continue;
-                }
-
-            next:
-                modifiedTemplate = modifiedTemplate.Append(current);
-                index += 1;
-            }
-
-            return modifiedTemplate.ToString();
+            var routeTemplate = TemplateParser.Parse(apiDescription.RelativePath);
+            var sanitizedSegments = routeTemplate
+                .Segments
+                .Select(s => string.Concat(s.Parts.Select(p => p.Name != null ? $"{{{p.Name}}}" : p.Text)));
+            return string.Join("/", sanitizedSegments);
         }
     }
 }
