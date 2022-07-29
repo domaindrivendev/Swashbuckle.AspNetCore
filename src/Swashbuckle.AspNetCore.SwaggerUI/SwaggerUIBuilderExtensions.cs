b3f8652c65e977b238da6ba8a271ed9e85164a17
penenkel <7736404+penenkel@users.noreply.github.com>
2022-07-29 10:27:29 +0200

    Add MapSwaggerUI for endpoint routing

--- src/Swashbuckle.AspNetCore.SwaggerUI/SwaggerUIBuilderExtensions.cs
+++ src/Swashbuckle.AspNetCore.SwaggerUI/SwaggerUIBuilderExtensions.cs
@@ -21,27 +21,65 @@ namespace Microsoft.AspNetCore.Builder
         }
 
         /// <summary>
-        /// Register the SwaggerUI middleware with optional setup action for DI-injected options
+        /// Register the SwaggerUI middleware with DI-injected options
         /// </summary>
+        /// <param name="setupAction">Optional action to modifiy the DI-injected options</param>
         public static IApplicationBuilder UseSwaggerUI(
             this IApplicationBuilder app,
             Action<SwaggerUIOptions> setupAction = null)
         {
+            SwaggerUIOptions options = FetchAndConfigureOptions(endpoints.ServiceProvider, setupAction);
+
+            return app.UseSwaggerUI(options);
+        }
+
+#if (!NETSTANDARD2_0)
+        /// <summary>
+        /// Register the SwaggerUI middleware with DI-injected options. Uses the endpoint routing syntax.
+        /// </summary>
+        /// <param name="setupAction">Optional action to modifiy the DI-injected options</param>
+        public static IEndpointConventionBuilder MapSwaggerUI(
+            this IEndpointRouteBuilder endpoints,
+            Action<SwaggerUIOptions>? setupAction = null)
+        {
+            SwaggerUIOptions options = FetchAndConfigureOptions(endpoints.ServiceProvider, setupAction);
+
+            var swaggerUiDelegate = endpoints.CreateApplicationBuilder()
+                .Use((context, next) =>
+                {
+                        // workaround for https://github.com/dotnet/aspnetcore/issues/24252
+                    context.SetEndpoint(null);
+                    return next();
+                })
+                .UseSwaggerUI(options)
+                .Build();
+
+            return endpoints.MapGet(options.RoutePrefix + "/{*wildcard}", swaggerUiDelegate);
+        }
+#endif
+
+        /// <summary>
+        /// Fetches the options instance registered with the DI, applies the local setup action and configures some defaults.
+        /// </summary>
+        private static SwaggerUIOptions FetchAndConfigureOptions(IServiceProvider serviceProvider, Action<SwaggerUIOptions>? setupAction = null)
+        {
+            // if a preconfigured instance of SwaggerUIOptions is registered with the DI, 
+            //      use it as a basis and then apply the local setup to it
             SwaggerUIOptions options;
-            using (var scope = app.ApplicationServices.CreateScope())
+            using (var scope = serviceProvider.CreateScope())
             {
                 options = scope.ServiceProvider.GetRequiredService<IOptionsSnapshot<SwaggerUIOptions>>().Value;
                 setupAction?.Invoke(options);
             }
 
-            // To simplify the common case, use a default that will work with the SwaggerMiddleware defaults
+            // To simplify the common case, use a default that will work with the SwaggerOptions and SwaggerGenOptions defaults
             if (options.ConfigObject.Urls == null)
             {
-                var hostingEnv = app.ApplicationServices.GetRequiredService<IWebHostEnvironment>();
+                var hostingEnv = serviceProvider.GetRequiredService<IWebHostEnvironment>();
                 options.ConfigObject.Urls = new[] { new UrlDescriptor { Name = $"{hostingEnv.ApplicationName} v1", Url = "v1/swagger.json" } };
             }
 
-            return app.UseSwaggerUI(options);
+            return options;
         }
     }
 }
