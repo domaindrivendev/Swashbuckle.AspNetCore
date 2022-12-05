57ea3ea14167d8474e3a5f8e72c9e80aa74c6f77
Yingxin Jiang <yingxin.jiang@softwire.com>
2022-12-05 17:30:17 +0000

    Add support for associating XML Comments with custom tags defined using TagsAttribute. Also includes all tags in root tags object, not just ones with XML Comments.

---
+++ test/Swashbuckle.AspNetCore.SwaggerGen.Test/Fixtures/FakeControllerWithCustomTagAndXmlSummary.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace Swashbuckle.AspNetCore.SwaggerGen.Test
+{
+    /// <summary>
+    /// Summary for FakeControllerWithCustomTag
+    /// </summary>
+    [Tags("fake controller custom tag")]
+    public class FakeControllerWithCustomTag
+    {
+        public void ActionAny()
+        { }
+
+        public void ActionAnother()
+        { }
+    }
+}
