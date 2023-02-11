1ac675c61daf4ca534af4090a1ba382fb610e052
James Cracknell <james.h.cracknell@gmail.com>
2023-02-11 12:23:48 -0500

    Added Swashbuckle.AspNetCore.Annotations.SwaggerIgnoreAttribute
    Application of the SwaggerIgnoreAttribute suppresses the annotated
    property during schema generation, without altering the serialization
    behavior as with JsonIgnoreAttribute.

---
+++ src/Swashbuckle.AspNetCore.SwaggerGen/Annotations/SwaggerIgnoreAttribute.cs
@@ -0,0 +1,16 @@
+using System;
+
+// ReSharper disable once CheckNamespace
+namespace Swashbuckle.AspNetCore.Annotations
+{
+    /// <summary>
+    /// Causes the annotated member to be ignored during schema generation.
+    /// Does not alter serialization behavior.
+    /// </summary>
+    /// <remarks>
+    /// Can be used in combination with <see cref="System.Text.Json.Serialization.JsonExtensionDataAttribute"/>
+    /// to capture and invalidate unsupported properties.
+    /// </remarks>
+    [AttributeUsage(AttributeTargets.Property)]
+    public class SwaggerIgnoreAttribute : Attribute { }
+}
