328db9cc0d988de24f81989e688f78cfc516e07b
martincostello <martin@martincostello.com>
2024-04-14 17:12:18 +0100

    Support native AoT with SwaggerUI
    Add support for using SwaggerUI with native AoT.
    SwaggerGen still does not support native AoT, though it happens to work for simple cases.
    Resolves #2550.

---
+++ src/Swashbuckle.AspNetCore.SwaggerUI/JavascriptStringEnumEnumerableConverter.cs
@@ -0,0 +1,67 @@
+﻿#if NET6_0_OR_GREATER
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Swashbuckle.AspNetCore.SwaggerUI;
+
+internal sealed class JavascriptStringEnumEnumerableConverter<[DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicFields)] TEnum>() :
+    JsonConverterFactory
+    where TEnum : struct, Enum
+{
+    private readonly JavascriptStringEnumConverter<TEnum> _enumConverter = new();
+
+    public override bool CanConvert(Type typeToConvert)
+        => typeToConvert.IsAssignableFrom(typeof(IEnumerable<TEnum>));
+
+    public override JsonConverter CreateConverter(Type typeToConvert, JsonSerializerOptions options)
+    {
+        if (!typeToConvert.IsAssignableFrom(typeof(IEnumerable<TEnum>)))
+        {
+            throw new NotSupportedException($"The type {typeToConvert} is not supported.");
+        }
+
+        var valueConverter = (JsonConverter<TEnum>)_enumConverter.CreateConverter(typeof(TEnum), options);
+        return new EnumEnumerableConverter(valueConverter);
+    }
+
+    private sealed class EnumEnumerableConverter(JsonConverter<TEnum> inner) : JsonConverter<IEnumerable<TEnum>>
+    {
+        public override IEnumerable<TEnum> Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            if (reader.TokenType != JsonTokenType.StartArray)
+            {
+                throw new JsonException("Expected start of a JSON array.");
+            }
+
+            var result = new List<TEnum>();
+
+            while (reader.Read())
+            {
+                if (reader.TokenType == JsonTokenType.EndArray)
+                {
+                    return result;
+                }
+
+                result.Add(inner.Read(ref reader, typeof(TEnum), options));
+            }
+
+            throw new JsonException("JSON array is malformed.");
+        }
+
+        public override void Write(Utf8JsonWriter writer, IEnumerable<TEnum> value, JsonSerializerOptions options)
+        {
+            writer.WriteStartArray();
+
+            foreach (var item in value)
+            {
+                inner.Write(writer, item, options);
+            }
+
+            writer.WriteEndArray();
+        }
+    }
+}
+#endif
---
+++ test/WebSites/WebApi.Aot/Program.cs
@@ -0,0 +1,51 @@
+using System.Text.Json.Serialization;
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateSlimBuilder(args);
+
+builder.Services.ConfigureHttpJsonOptions(options =>
+{
+    options.SerializerOptions.TypeInfoResolverChain.Insert(0, AppJsonSerializerContext.Default);
+});
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(configure =>
+{
+    configure.SwaggerDoc(
+        "v1",
+        new OpenApiInfo
+        {
+            Title = "Native AoT API V1",
+            Version = "v1",
+            Description = "A sample API for testing Swashbuckle with native AoT",
+            TermsOfService = new Uri("http://tempuri.org/terms")
+        });
+});
+
+var app = builder.Build();
+
+var sampleTodos = new Todo[] {
+    new(1, "Walk the dog"),
+    new(2, "Do the dishes", DateOnly.FromDateTime(DateTime.Now)),
+    new(3, "Do the laundry", DateOnly.FromDateTime(DateTime.Now.AddDays(1))),
+    new(4, "Clean the bathroom"),
+    new(5, "Clean the car", DateOnly.FromDateTime(DateTime.Now.AddDays(2)))
+};
+
+var todosApi = app.MapGroup("/todos");
+todosApi.MapGet("/", () => sampleTodos);
+todosApi.MapGet("/{id}", (int id) =>
+    sampleTodos.FirstOrDefault(a => a.Id == id) is { } todo
+        ? Results.Ok(todo)
+        : Results.NotFound());
+
+app.UseSwagger();
+app.UseSwaggerUI();
+app.MapSwagger();
+
+app.Run();
+
+internal record Todo(int Id, string? Title, DateOnly? DueBy = null, bool IsComplete = false);
+
+[JsonSerializable(typeof(Todo[]))]
+internal partial class AppJsonSerializerContext : JsonSerializerContext;
