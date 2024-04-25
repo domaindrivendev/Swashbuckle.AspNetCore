53eba1e65f648c956e931b41832e3602c1a9a9ab
remcolam <remcolam@users.noreply.github.com>
2024-04-25 10:42:22 +0200

    Update src/Swashbuckle.AspNetCore.Swagger/DependencyInjection/SwaggerServiceCollectionExtensions.cs
    Co-authored-by: Martin Costello <martin@martincostello.com>

--- src/Swashbuckle.AspNetCore.Swagger/DependencyInjection/SwaggerServiceCollectionExtensions.cs
+++ src/Swashbuckle.AspNetCore.Swagger/DependencyInjection/SwaggerServiceCollectionExtensions.cs
@@ -4,15 +4,15 @@ using Swashbuckle.AspNetCore.Swagger;
 namespace Microsoft.Extensions.DependencyInjection;
 
 /// <summary>
-/// Extensions to configure swagger in the service collection
+/// Extensions to configure dependencies for Swagger.
 /// </summary>
 public static class SwaggerServiceCollectionExtensions
 {
     /// <summary>
-    /// Configure swagger options in the service collection.
+    /// Configures Swagger options in the specified service collection.
     /// </summary>
-    /// <param name="services">The service collection to set the swagger options on.</param>
-    /// <param name="setupAction">The setup action for swagger.</param>
+    /// <param name="services">The service collection to configure the Swagger options for.</param>
+    /// <param name="setupAction">A delegate to a method to use to configure the Swagger options.</param>
     public static void ConfigureSwagger(
         this IServiceCollection services,
         Action<SwaggerOptions> setupAction)
