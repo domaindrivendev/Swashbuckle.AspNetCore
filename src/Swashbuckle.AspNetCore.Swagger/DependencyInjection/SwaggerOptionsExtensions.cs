e2048bf32c3bdb1b2cdd6e4084c8f0816dc1a193
Remco Lam <remco.lam@rabobank.com>
2024-04-25 10:31:23 +0200

    Fixed comments

--- src/Swashbuckle.AspNetCore.Swagger/DependencyInjection/SwaggerOptionsExtensions.cs
+++ src/Swashbuckle.AspNetCore.Swagger/DependencyInjection/SwaggerOptionsExtensions.cs
@@ -12,9 +12,9 @@ public static class SwaggerOptionsExtensions
     /// Sets a custom Swagger document serializer to use.
     /// </summary>
     /// <remarks>For the CLI tool to be able to use this, this needs to be configured for use in the service collection of your application.</remarks>
-    /// <typeparam name="TDocumentSerializer">The custom Swagger document serializer class</typeparam>
-    /// <param name="swaggerOptions">The options to configure for serializer for.</param>
-    /// <param name="constructorParameters">Theparameters to pass into the constructor of the custom Swagger document serializer implementation.</param>
+    /// <typeparam name="TDocumentSerializer">The custom Swagger document serializer implementation.</typeparam>
+    /// <param name="swaggerOptions">The options to configure the serializer for.</param>
+    /// <param name="constructorParameters">The parameters to pass into the constructor of the custom Swagger document serializer implementation.</param>
     public static void SetCustomDocumentSerializer<TDocumentSerializer>(
         this SwaggerOptions swaggerOptions,
         params object[] constructorParameters)
