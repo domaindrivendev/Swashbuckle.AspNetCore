89cc7f9238a6cecd188c9ff30585faad16d29d9c
Remco Lam <remco.lam@rabobank.com>
2024-04-26 11:03:08 +0200

    Added tests for new extension methods

--- test/Swashbuckle.AspNetCore.SwaggerGen.Test/ConfigureSwaggerGeneratorOptionsTests.cs
+++ test/Swashbuckle.AspNetCore.SwaggerGen.Test/ConfigureSwaggerGeneratorOptionsTests.cs
@@ -2,7 +2,6 @@
 using System.Reflection;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting.Internal;
 using Microsoft.Extensions.Options;
 using NSubstitute;
 using Xunit;
@@ -21,6 +20,153 @@ public static class ConfigureSwaggerGeneratorOptionsTests
         // to SwaggerGeneratorOptions and ConfigureSwaggerGeneratorOptions.DeepCopy() needs to be updated
         Assert.Equal(18, publicProperties.Length);
     }
+
+    [Fact]
+    public static void AddingParameterFilterInstances_WhenConfiguringOptions_SameInstanceIsAdded()
+    {
+        var webhostingEnvironment = Substitute.For<IWebHostEnvironment>();
+        webhostingEnvironment.ApplicationName.Returns("Swashbuckle.AspNetCore.SwaggerGen.Test");
+
+        var testParameterFilter = new TestParameterFilter();
+
+        var options = new SwaggerGenOptions();
+        options.AddParameterFilterInstance(testParameterFilter);
+        options.AddParameterFilterInstance(testParameterFilter);
+
+        var configureSwaggerGeneratorOptions = new ConfigureSwaggerGeneratorOptions(
+            Options.Create(options),
+            null,
+            webhostingEnvironment);
+        var swaggerGeneratorOptions = new SwaggerGeneratorOptions();
+
+        configureSwaggerGeneratorOptions.Configure(swaggerGeneratorOptions);
+
+        Assert.Equal(2, swaggerGeneratorOptions.ParameterFilters.Count);
+        Assert.Same(testParameterFilter, swaggerGeneratorOptions.ParameterFilters.First());
+        Assert.Same(testParameterFilter, swaggerGeneratorOptions.ParameterFilters.Last());
+    }
+
+
+    [Fact]
+    public static void AddingParameterFilterTypes_WhenConfiguringOptions_DifferentInstancesAreAdded()
+    {
+        var webhostingEnvironment = Substitute.For<IWebHostEnvironment>();
+        webhostingEnvironment.ApplicationName.Returns("Swashbuckle.AspNetCore.SwaggerGen.Test");
+
+        var options = new SwaggerGenOptions();
+        options.ParameterFilter<TestParameterFilter>();
+        options.ParameterFilter<TestParameterFilter>();
+
+        var configureSwaggerGeneratorOptions = new ConfigureSwaggerGeneratorOptions(
+            Options.Create(options),
+            null,
+            webhostingEnvironment);
+        var swaggerGeneratorOptions = new SwaggerGeneratorOptions();
+
+        configureSwaggerGeneratorOptions.Configure(swaggerGeneratorOptions);
+
+        Assert.Equal(2, swaggerGeneratorOptions.ParameterFilters.Count);
+        Assert.NotSame(swaggerGeneratorOptions.ParameterFilters.First(), swaggerGeneratorOptions.ParameterFilters.Last());
+    }
+
+
+    [Fact]
+    public static void AddingRequestBodyFilterInstances_WhenConfiguringOptions_SameInstanceIsAdded()
+    {
+        var webhostingEnvironment = Substitute.For<IWebHostEnvironment>();
+        webhostingEnvironment.ApplicationName.Returns("Swashbuckle.AspNetCore.SwaggerGen.Test");
+
+        var testRequestBodyFilter = new TestRequestBodyFilter();
+
+        var options = new SwaggerGenOptions();
+        options.AddRequestBodyFilterInstance(testRequestBodyFilter);
+        options.AddRequestBodyFilterInstance(testRequestBodyFilter);
+
+        var configureSwaggerGeneratorOptions = new ConfigureSwaggerGeneratorOptions(
+            Options.Create(options),
+            null,
+            webhostingEnvironment);
+        var swaggerGeneratorOptions = new SwaggerGeneratorOptions();
+
+        configureSwaggerGeneratorOptions.Configure(swaggerGeneratorOptions);
+
+        Assert.Equal(2, swaggerGeneratorOptions.RequestBodyFilters.Count);
+        Assert.Same(testRequestBodyFilter, swaggerGeneratorOptions.RequestBodyFilters.First());
+        Assert.Same(testRequestBodyFilter, swaggerGeneratorOptions.RequestBodyFilters.Last());
+    }
+
+
+    [Fact]
+    public static void AddingRequestBodyFilterTypes_WhenConfiguringOptions_DifferentInstancesAreAdded()
+    {
+        var webhostingEnvironment = Substitute.For<IWebHostEnvironment>();
+        webhostingEnvironment.ApplicationName.Returns("Swashbuckle.AspNetCore.SwaggerGen.Test");
+
+        var options = new SwaggerGenOptions();
+        options.RequestBodyFilter<TestRequestBodyFilter>();
+        options.RequestBodyFilter<TestRequestBodyFilter>();
+
+        var configureSwaggerGeneratorOptions = new ConfigureSwaggerGeneratorOptions(
+            Options.Create(options),
+            null,
+            webhostingEnvironment);
+        var swaggerGeneratorOptions = new SwaggerGeneratorOptions();
+
+        configureSwaggerGeneratorOptions.Configure(swaggerGeneratorOptions);
+
+        Assert.Equal(2, swaggerGeneratorOptions.RequestBodyFilters.Count);
+        Assert.NotSame(swaggerGeneratorOptions.RequestBodyFilters.First(), swaggerGeneratorOptions.RequestBodyFilters.Last());
+    }
+
+
+    [Fact]
+    public static void AddingOperationFilterInstances_WhenConfiguringOptions_SameInstanceIsAdded()
+    {
+        var webhostingEnvironment = Substitute.For<IWebHostEnvironment>();
+        webhostingEnvironment.ApplicationName.Returns("Swashbuckle.AspNetCore.SwaggerGen.Test");
+
+        var testOperationFilter = new TestOperationFilter();
+
+        var options = new SwaggerGenOptions();
+        options.AddOperationFilterInstance(testOperationFilter);
+        options.AddOperationFilterInstance(testOperationFilter);
+
+        var configureSwaggerGeneratorOptions = new ConfigureSwaggerGeneratorOptions(
+            Options.Create(options),
+            null,
+            webhostingEnvironment);
+        var swaggerGeneratorOptions = new SwaggerGeneratorOptions();
+
+        configureSwaggerGeneratorOptions.Configure(swaggerGeneratorOptions);
+
+        Assert.Equal(2, swaggerGeneratorOptions.OperationFilters.Count);
+        Assert.Same(testOperationFilter, swaggerGeneratorOptions.OperationFilters.First());
+        Assert.Same(testOperationFilter, swaggerGeneratorOptions.OperationFilters.Last());
+    }
+
+
+    [Fact]
+    public static void AddingOperationFilterTypes_WhenConfiguringOptions_DifferentInstancesAreAdded()
+    {
+        var webhostingEnvironment = Substitute.For<IWebHostEnvironment>();
+        webhostingEnvironment.ApplicationName.Returns("Swashbuckle.AspNetCore.SwaggerGen.Test");
+
+        var options = new SwaggerGenOptions();
+        options.OperationFilter<TestOperationFilter>();
+        options.OperationFilter<TestOperationFilter>();
+
+        var configureSwaggerGeneratorOptions = new ConfigureSwaggerGeneratorOptions(
+            Options.Create(options),
+            null,
+            webhostingEnvironment);
+        var swaggerGeneratorOptions = new SwaggerGeneratorOptions();
+
+        configureSwaggerGeneratorOptions.Configure(swaggerGeneratorOptions);
+
+        Assert.Equal(2, swaggerGeneratorOptions.OperationFilters.Count);
+        Assert.NotSame(swaggerGeneratorOptions.OperationFilters.First(), swaggerGeneratorOptions.OperationFilters.Last());
+    }
+
 
     [Fact]
     public static void AddingDocumentFilterInstances_WhenConfiguringOptions_SameInstanceIsAdded()
@@ -49,16 +195,14 @@ public static class ConfigureSwaggerGeneratorOptionsTests
 
 
     [Fact]
-    public static void AddingDocumentFilters_WhenConfiguringOption_DifferentInstanceIsAdded()
+    public static void AddingDocumentFilterTypes_WhenConfiguringOptions_DifferentInstancesAreAdded()
     {
         var webhostingEnvironment = Substitute.For<IWebHostEnvironment>();
         webhostingEnvironment.ApplicationName.Returns("Swashbuckle.AspNetCore.SwaggerGen.Test");
 
-        var testDocumentFilter = new TestDocumentFilter();
-
         var options = new SwaggerGenOptions();
-        options.AddDocumentFilterInstance(testDocumentFilter);
-        options.AddDocumentFilterInstance(testDocumentFilter);
+        options.DocumentFilter<TestDocumentFilter>();
+        options.DocumentFilter<TestDocumentFilter>();
 
         var configureSwaggerGeneratorOptions = new ConfigureSwaggerGeneratorOptions(
             Options.Create(options),
@@ -69,7 +213,6 @@ public static class ConfigureSwaggerGeneratorOptionsTests
         configureSwaggerGeneratorOptions.Configure(swaggerGeneratorOptions);
 
         Assert.Equal(2, swaggerGeneratorOptions.DocumentFilters.Count);
-        Assert.Same(testDocumentFilter, swaggerGeneratorOptions.DocumentFilters.First());
-        Assert.Same(testDocumentFilter, swaggerGeneratorOptions.DocumentFilters.Last());
+        Assert.NotSame(swaggerGeneratorOptions.DocumentFilters.First(), swaggerGeneratorOptions.DocumentFilters.Last());
     }
 }
