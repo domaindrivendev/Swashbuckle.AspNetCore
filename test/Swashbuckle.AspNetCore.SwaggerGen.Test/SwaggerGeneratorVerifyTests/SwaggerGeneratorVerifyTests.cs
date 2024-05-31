e3542bcdef68de1fb9666e642c4cfedb1b4104e4
Keah Peters <keahpeters@hotmail.co.uk>
2024-05-31 17:09:00 +0100

    Code review fixes

--- test/Swashbuckle.AspNetCore.SwaggerGen.Test/SwaggerGeneratorVerifyTests/SwaggerGeneratorVerifyTests.cs
+++ test/Swashbuckle.AspNetCore.SwaggerGen.Test/SwaggerGeneratorVerifyTests/SwaggerGeneratorVerifyTests.cs
@@ -14,234 +14,233 @@ using Swashbuckle.AspNetCore.TestSupport;
 using VerifyXunit;
 using Xunit;
 
-namespace Swashbuckle.AspNetCore.SwaggerGen.Test
+namespace Swashbuckle.AspNetCore.SwaggerGen.Test;
+
+public class SwaggerGeneratorVerifyTests
 {
-    public class SwaggerGeneratorVerifyTests
+    [Fact]
+    public Task ApiDescriptionsWithMatchingGroupName()
     {
-        [Fact]
-        public Task ApiDescriptionsWithMatchingGroupName()
-        {
-            var subject = Subject(
-                apiDescriptions: new[]
-                {
-                    ApiDescriptionFactory.Create<FakeController>(
-                        c => nameof(c.ActionWithNoParameters), groupName: "v1", httpMethod: "POST", relativePath: "resource"),
-
-                    ApiDescriptionFactory.Create<FakeController>(
-                        c => nameof(c.ActionWithNoParameters), groupName: "v1", httpMethod: "GET", relativePath: "resource"),
-
-                    ApiDescriptionFactory.Create<FakeController>(
-                        c => nameof(c.ActionWithNoParameters), groupName: "v2", httpMethod: "POST", relativePath: "resource"),
-                },
-                options: new SwaggerGeneratorOptions
-                {
-                    SwaggerDocs = new Dictionary<string, OpenApiInfo>
-                    {
-                        ["v1"] = new OpenApiInfo { Version = "V1", Title = "Test API" }
-                    }
-                }
-            );
-
-            var document = subject.GetSwagger("v1");
-
-            return Verifier.Verify(document);
-        }
-
-        [Fact]
-        public Task ActionWithRouteNameMetadata()
-        {
-            var subject = Subject(
-                apiDescriptions: new[]
-                {
-                    ApiDescriptionFactory.Create<FakeController>(
-                        c => nameof(c.ActionWithRouteNameMetadata), groupName: "v1", httpMethod: "POST", relativePath: "resource"),
-                }
-            );
-
-            var document = subject.GetSwagger("v1");
-
-            return Verifier.Verify(document);
-        }
-
-        [Fact]
-        public Task ActionWithEndpointNameMetadata()
-        {
-            var methodInfo = typeof(FakeController).GetMethod(nameof(FakeController.ActionWithParameter));
-            var actionDescriptor = new ActionDescriptor
+        var subject = Subject(
+            apiDescriptions: new[]
             {
-                EndpointMetadata = new List<object>() { new EndpointNameMetadata("SomeEndpointName") },
-                RouteValues = new Dictionary<string, string>
-                {
-                    ["controller"] = methodInfo.DeclaringType.Name.Replace("Controller", string.Empty)
-                }
-            };
-            var subject = Subject(
-                apiDescriptions: new[]
-                {
-                    ApiDescriptionFactory.Create(actionDescriptor, methodInfo, groupName: "v1", httpMethod: "POST", relativePath: "resource"),
-                }
-            );
+                ApiDescriptionFactory.Create<FakeController>(
+                    c => nameof(c.ActionWithNoParameters), groupName: "v1", httpMethod: "POST", relativePath: "resource"),
 
-            var document = subject.GetSwagger("v1");
+                ApiDescriptionFactory.Create<FakeController>(
+                    c => nameof(c.ActionWithNoParameters), groupName: "v1", httpMethod: "GET", relativePath: "resource"),
 
-            return Verifier.Verify(document);
-        }
-
-        [Fact]
-        public Task ActionWithProvidedOpenApiMetadata()
-        {
-            var methodInfo = typeof(FakeController).GetMethod(nameof(FakeController.ActionWithParameter));
-            var actionDescriptor = new ActionDescriptor
+                ApiDescriptionFactory.Create<FakeController>(
+                    c => nameof(c.ActionWithNoParameters), groupName: "v2", httpMethod: "POST", relativePath: "resource"),
+            },
+            options: new SwaggerGeneratorOptions
             {
-                EndpointMetadata = new List<object>()
+                SwaggerDocs = new Dictionary<string, OpenApiInfo>
                 {
-                    new OpenApiOperation
-                    {
-                        OperationId = "OperationIdSetInMetadata",
-                        Parameters = new List<OpenApiParameter>()
-                        {
-                            new OpenApiParameter
-                            {
-                                Name = "ParameterInMetadata"
-                            }
-                        }
-                    }
-                },
-                RouteValues = new Dictionary<string, string>
-                {
-                    ["controller"] = methodInfo.DeclaringType.Name.Replace("Controller", string.Empty)
+                    ["v1"] = new OpenApiInfo { Version = "V1", Title = "Test API" }
                 }
-            };
-            var subject = Subject(
-                apiDescriptions: new[]
-                {
-                    ApiDescriptionFactory.Create(actionDescriptor, methodInfo, groupName: "v1", httpMethod: "POST", relativePath: "resource"),
-                }
-            );
+            }
+        );
 
-            var document = subject.GetSwagger("v1");
+        var document = subject.GetSwagger("v1");
 
-            return Verifier.Verify(document);
-        }
+        return Verifier.Verify(document);
+    }
 
-        [Theory]
-        [InlineData(nameof(FakeController.ActionWithAcceptFromHeaderParameter))]
-        [InlineData(nameof(FakeController.ActionWithContentTypeFromHeaderParameter))]
-        [InlineData(nameof(FakeController.ActionWithAuthorizationFromHeaderParameter))]
-        public Task ActionsWithIllegalHeaderParameters(string action)
-        {
-            var illegalParameter = typeof(FakeController).GetMethod(action).GetParameters()[0];
-            var fromHeaderAttribute = illegalParameter.GetCustomAttribute<FromHeaderAttribute>();
-
-            var subject = Subject(
-                new[]
-                {
-                    ApiDescriptionFactory.Create<FakeController>(
-                        c => action,
-                        groupName: "v1",
-                        httpMethod: "GET",
-                        relativePath: "resource",
-                        parameterDescriptions: new[]
-                        {
-                            new ApiParameterDescription
-                            {
-                                Name = fromHeaderAttribute?.Name ?? illegalParameter.Name,
-                                Source = BindingSource.Header,
-                                ModelMetadata = ModelMetadataFactory.CreateForParameter(illegalParameter)
-                            },
-                            new ApiParameterDescription
-                            {
-                                Name = "param",
-                                Source = BindingSource.Header
-                            }
-                        }
-                    )
-                }
-            );
-
-            var document = subject.GetSwagger("v1");
-
-            return Verifier.Verify(document).UseParameters(action);
-        }
-
-        [Theory]
-        [InlineData(nameof(FakeController.ActionWithParameterWithRequiredAttribute))]
-        [InlineData(nameof(FakeController.ActionWithParameterWithBindRequiredAttribute))]
-        public Task ActionWithRequiredParameter(string action)
-        {
-            var subject = Subject(
-                apiDescriptions: new[]
-                {
-                    ApiDescriptionFactory.Create(
-                        methodInfo: typeof(FakeController).GetMethod(action),
-                        groupName: "v1",
-                        httpMethod: "POST",
-                        relativePath: "resource",
-                        parameterDescriptions: new []
-                        {
-                            new ApiParameterDescription
-                            {
-                                Name = "param",
-                                Source = BindingSource.Query
-                            }
-                        })
-                }
-            );
-
-            var document = subject.GetSwagger("v1");
-
-            return Verifier.Verify(document).UseParameters(action);
-        }
-
-#if NET7_0_OR_GREATER
-        [Fact]
-        public Task ActionWithRequiredMember()
-        {
-            var subject = Subject(
-                apiDescriptions: new[]
-                {
-                    ApiDescriptionFactory.Create(
-                        methodInfo: typeof(FakeController).GetMethod(nameof(FakeController.ActionWithRequiredMember)),
-                        groupName: "v1",
-                        httpMethod: "POST",
-                        relativePath: "resource",
-                        parameterDescriptions: new []
-                        {
-                            new ApiParameterDescription
-                            {
-                                Name = "param",
-                                Source = BindingSource.Query,
-                                ModelMetadata = ModelMetadataFactory.CreateForProperty(typeof(FakeController.TypeWithRequiredProperty), "RequiredProperty")
-                            }
-                        })
-                }
-            );
-
-            var document = subject.GetSwagger("v1");
-
-            return Verifier.Verify(document);
-        }
-#endif
-
-        private static SwaggerGenerator Subject(
-            IEnumerable<ApiDescription> apiDescriptions,
-            SwaggerGeneratorOptions options = null,
-            IEnumerable<AuthenticationScheme> authenticationSchemes = null)
-        {
-            return new SwaggerGenerator(
-                options ?? DefaultOptions,
-                new FakeApiDescriptionGroupCollectionProvider(apiDescriptions),
-                new SchemaGenerator(new SchemaGeneratorOptions(), new JsonSerializerDataContractResolver(new JsonSerializerOptions())),
-                new FakeAuthenticationSchemeProvider(authenticationSchemes ?? Enumerable.Empty<AuthenticationScheme>())
-            );
-        }
-
-        private static readonly SwaggerGeneratorOptions DefaultOptions = new SwaggerGeneratorOptions
-        {
-            SwaggerDocs = new Dictionary<string, OpenApiInfo>
+    [Fact]
+    public Task ActionWithRouteNameMetadata()
+    {
+        var subject = Subject(
+            apiDescriptions: new[]
             {
-                ["v1"] = new OpenApiInfo { Version = "V1", Title = "Test API" }
+                ApiDescriptionFactory.Create<FakeController>(
+                    c => nameof(c.ActionWithRouteNameMetadata), groupName: "v1", httpMethod: "POST", relativePath: "resource"),
+            }
+        );
+
+        var document = subject.GetSwagger("v1");
+
+        return Verifier.Verify(document);
+    }
+
+    [Fact]
+    public Task ActionWithEndpointNameMetadata()
+    {
+        var methodInfo = typeof(FakeController).GetMethod(nameof(FakeController.ActionWithParameter));
+        var actionDescriptor = new ActionDescriptor
+        {
+            EndpointMetadata = new List<object>() { new EndpointNameMetadata("SomeEndpointName") },
+            RouteValues = new Dictionary<string, string>
+            {
+                ["controller"] = methodInfo.DeclaringType.Name.Replace("Controller", string.Empty)
             }
         };
+        var subject = Subject(
+            apiDescriptions: new[]
+            {
+                ApiDescriptionFactory.Create(actionDescriptor, methodInfo, groupName: "v1", httpMethod: "POST", relativePath: "resource"),
+            }
+        );
+
+        var document = subject.GetSwagger("v1");
+
+        return Verifier.Verify(document);
     }
+
+    [Fact]
+    public Task ActionWithProvidedOpenApiMetadata()
+    {
+        var methodInfo = typeof(FakeController).GetMethod(nameof(FakeController.ActionWithParameter));
+        var actionDescriptor = new ActionDescriptor
+        {
+            EndpointMetadata = new List<object>()
+            {
+                new OpenApiOperation
+                {
+                    OperationId = "OperationIdSetInMetadata",
+                    Parameters = new List<OpenApiParameter>()
+                    {
+                        new OpenApiParameter
+                        {
+                            Name = "ParameterInMetadata"
+                        }
+                    }
+                }
+            },
+            RouteValues = new Dictionary<string, string>
+            {
+                ["controller"] = methodInfo.DeclaringType.Name.Replace("Controller", string.Empty)
+            }
+        };
+        var subject = Subject(
+            apiDescriptions: new[]
+            {
+                ApiDescriptionFactory.Create(actionDescriptor, methodInfo, groupName: "v1", httpMethod: "POST", relativePath: "resource"),
+            }
+        );
+
+        var document = subject.GetSwagger("v1");
+
+        return Verifier.Verify(document);
+    }
+
+    [Theory]
+    [InlineData(nameof(FakeController.ActionWithAcceptFromHeaderParameter))]
+    [InlineData(nameof(FakeController.ActionWithContentTypeFromHeaderParameter))]
+    [InlineData(nameof(FakeController.ActionWithAuthorizationFromHeaderParameter))]
+    public Task ActionsWithIllegalHeaderParameters(string action)
+    {
+        var illegalParameter = typeof(FakeController).GetMethod(action).GetParameters()[0];
+        var fromHeaderAttribute = illegalParameter.GetCustomAttribute<FromHeaderAttribute>();
+
+        var subject = Subject(
+            new[]
+            {
+                ApiDescriptionFactory.Create<FakeController>(
+                    c => action,
+                    groupName: "v1",
+                    httpMethod: "GET",
+                    relativePath: "resource",
+                    parameterDescriptions: new[]
+                    {
+                        new ApiParameterDescription
+                        {
+                            Name = fromHeaderAttribute?.Name ?? illegalParameter.Name,
+                            Source = BindingSource.Header,
+                            ModelMetadata = ModelMetadataFactory.CreateForParameter(illegalParameter)
+                        },
+                        new ApiParameterDescription
+                        {
+                            Name = "param",
+                            Source = BindingSource.Header
+                        }
+                    }
+                )
+            }
+        );
+
+        var document = subject.GetSwagger("v1");
+
+        return Verifier.Verify(document).UseParameters(action);
+    }
+
+    [Theory]
+    [InlineData(nameof(FakeController.ActionWithParameterWithRequiredAttribute))]
+    [InlineData(nameof(FakeController.ActionWithParameterWithBindRequiredAttribute))]
+    public Task ActionWithRequiredParameter(string action)
+    {
+        var subject = Subject(
+            apiDescriptions: new[]
+            {
+                ApiDescriptionFactory.Create(
+                    methodInfo: typeof(FakeController).GetMethod(action),
+                    groupName: "v1",
+                    httpMethod: "POST",
+                    relativePath: "resource",
+                    parameterDescriptions: new []
+                    {
+                        new ApiParameterDescription
+                        {
+                            Name = "param",
+                            Source = BindingSource.Query
+                        }
+                    })
+            }
+        );
+
+        var document = subject.GetSwagger("v1");
+
+        return Verifier.Verify(document).UseParameters(action);
+    }
+
+#if NET7_0_OR_GREATER
+    [Fact]
+    public Task ActionWithRequiredMember()
+    {
+        var subject = Subject(
+            apiDescriptions: new[]
+            {
+                ApiDescriptionFactory.Create(
+                    methodInfo: typeof(FakeController).GetMethod(nameof(FakeController.ActionWithRequiredMember)),
+                    groupName: "v1",
+                    httpMethod: "POST",
+                    relativePath: "resource",
+                    parameterDescriptions: new []
+                    {
+                        new ApiParameterDescription
+                        {
+                            Name = "param",
+                            Source = BindingSource.Query,
+                            ModelMetadata = ModelMetadataFactory.CreateForProperty(typeof(FakeController.TypeWithRequiredProperty), "RequiredProperty")
+                        }
+                    })
+            }
+        );
+
+        var document = subject.GetSwagger("v1");
+
+        return Verifier.Verify(document);
+    }
+#endif
+
+    private static SwaggerGenerator Subject(
+        IEnumerable<ApiDescription> apiDescriptions,
+        SwaggerGeneratorOptions options = null,
+        IEnumerable<AuthenticationScheme> authenticationSchemes = null)
+    {
+        return new SwaggerGenerator(
+            options ?? DefaultOptions,
+            new FakeApiDescriptionGroupCollectionProvider(apiDescriptions),
+            new SchemaGenerator(new SchemaGeneratorOptions(), new JsonSerializerDataContractResolver(new JsonSerializerOptions())),
+            new FakeAuthenticationSchemeProvider(authenticationSchemes ?? Enumerable.Empty<AuthenticationScheme>())
+        );
+    }
+
+    private static readonly SwaggerGeneratorOptions DefaultOptions = new SwaggerGeneratorOptions
+    {
+        SwaggerDocs = new Dictionary<string, OpenApiInfo>
+        {
+            ["v1"] = new OpenApiInfo { Version = "V1", Title = "Test API" }
+        }
+    };
 }
