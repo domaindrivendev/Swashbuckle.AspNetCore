31542561ad06f70b9197766ada8c0ca7dcf05c23
Javier García de la Noceda Argüelles <jgarcian@riamoneytransfer.com>
2024-07-15 19:04:15 +0200

    Add more test on MethodInfoExtensions

--- test/Swashbuckle.AspNetCore.SwaggerGen.Test/Fixtures/FakeGenericResourceController.cs
+++ test/Swashbuckle.AspNetCore.SwaggerGen.Test/Fixtures/FakeGenericResourceController.cs
@@ -8,15 +8,18 @@ namespace Swashbuckle.AspNetCore.SwaggerGen.Test.Fixtures
     internal class FakeGenericResourceController<T>
         where T : class
     {
-        public virtual int Create([FromBody, Required] T t, CancellationToken cancellationToken) => 1;
-        public virtual int Create([FromBody, Required] string key, T t) => 1;
-        public virtual int Create([FromBody, Required] T[] ts) => 1;
-        public virtual int Create([FromBody, Required] string[] strings) => 1;
+        public virtual int DifferentMethodsSignatures([FromBody, Required] T t, CancellationToken cancellationToken) => 1;
+        public virtual int DifferentMethodsSignatures([FromBody, Required] string key, T t) => 1;
+        public virtual int DifferentMethodsSignatures([FromBody, Required] T[] ts) => 1;
+        public virtual int DifferentMethodsSignatures([FromBody, Required] string[] strings) => 1;
 
-        public virtual int Create([FromBody, Required] List<T> ts) => 1;
-        public virtual int Create([FromBody, Required] List<string> strings) => 1;
+        public virtual int DifferentMethodsSignatures([FromBody, Required] List<T> ts) => 1;
+        public virtual int DifferentMethodsSignatures([FromBody, Required] List<string> strings) => 1;
 
-        public virtual int Create([FromBody, Required] Dictionary<int, T> ts) => 1;
+        public virtual int DifferentMethodsSignatures([FromBody, Required] Dictionary<int, T> ts) => 1;
+
+        public virtual int SimilarMethodsSignatures([FromBody, Required] List<T> ts) => 1;
+        public virtual int SimilarMethodsSignatures([FromBody, Required] IEnumerable<T> ts) => 1;
     }
     internal class NonGenericResourceController : FakeGenericResourceController<XmlAnnotatedRecord>
     {
--- test/Swashbuckle.AspNetCore.SwaggerGen.Test/XmlComments/XmlMethodInfoExtensionsTests.cs
+++ test/Swashbuckle.AspNetCore.SwaggerGen.Test/XmlComments/XmlMethodInfoExtensionsTests.cs
@@ -7,16 +7,30 @@ namespace Swashbuckle.AspNetCore.SwaggerGen.Test
 {
     public class XmlMethodInfoExtensionsTests
     {
-        [Theory, MemberData(nameof(Methods))]
-        public void Test(MethodInfo methodInfo)
+        [Theory, MemberData(nameof(DifferentMethodsSignatures))]
+        public void DifferentMethodsSignatures_ShouldNotBeNull(MethodInfo methodInfo)
         {
             Assert.NotNull(methodInfo.GetUnderlyingGenericTypeMethod());
         }
-        public static TheoryData<MethodInfo> Methods =>
+
+        [Theory, MemberData(nameof(SimilarMethodsSignatures))]
+        public void SimilarMethodsSignatures_ShouldBeNull(MethodInfo methodInfo)
+        {
+            Assert.Null(methodInfo.GetUnderlyingGenericTypeMethod());
+        }
+
+        public static TheoryData<MethodInfo> DifferentMethodsSignatures =>
             new(typeof(NonGenericResourceController).GetMethods()
                 .Where(s =>
                 {
-                    return s.Name == nameof(NonGenericResourceController.Create);
+                    return s.Name == nameof(NonGenericResourceController.DifferentMethodsSignatures);
+                }));
+
+        public static TheoryData<MethodInfo> SimilarMethodsSignatures =>
+            new(typeof(NonGenericResourceController).GetMethods()
+                .Where(s =>
+                {
+                    return s.Name == nameof(NonGenericResourceController.SimilarMethodsSignatures);
                 }));
     }
 }
