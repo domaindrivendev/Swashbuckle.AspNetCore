4dcb99b35571ce32d75046f9678ab96c7bd30768
Javier García de la Noceda Argüelles <jgarcian@riamoneytransfer.com>
2024-07-15 18:57:23 +0200

    UnitTest for MethodInfoExtensions

---
+++ test/Swashbuckle.AspNetCore.SwaggerGen.Test/Fixtures/FakeGenericResourceController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Threading;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Swashbuckle.AspNetCore.SwaggerGen.Test.Fixtures
+{
+    internal class FakeGenericResourceController<T>
+        where T : class
+    {
+        public virtual int Create([FromBody, Required] T t, CancellationToken cancellationToken) => 1;
+        public virtual int Create([FromBody, Required] string key, T t) => 1;
+        public virtual int Create([FromBody, Required] T[] ts) => 1;
+        public virtual int Create([FromBody, Required] string[] strings) => 1;
+
+        public virtual int Create([FromBody, Required] List<T> ts) => 1;
+        public virtual int Create([FromBody, Required] List<string> strings) => 1;
+
+        public virtual int Create([FromBody, Required] Dictionary<int, T> ts) => 1;
+    }
+    internal class NonGenericResourceController : FakeGenericResourceController<XmlAnnotatedRecord>
+    {
+
+    }
+}
---
+++ test/Swashbuckle.AspNetCore.SwaggerGen.Test/XmlComments/XmlMethodInfoExtensionsTests.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+using System.Reflection;
+using Swashbuckle.AspNetCore.SwaggerGen.Test.Fixtures;
+using Xunit;
+
+namespace Swashbuckle.AspNetCore.SwaggerGen.Test
+{
+    public class XmlMethodInfoExtensionsTests
+    {
+        [Theory, MemberData(nameof(Methods))]
+        public void Test(MethodInfo methodInfo)
+        {
+            Assert.NotNull(methodInfo.GetUnderlyingGenericTypeMethod());
+        }
+        public static TheoryData<MethodInfo> Methods =>
+            new(typeof(NonGenericResourceController).GetMethods()
+                .Where(s =>
+                {
+                    return s.Name == nameof(NonGenericResourceController.Create);
+                }));
+    }
+}
