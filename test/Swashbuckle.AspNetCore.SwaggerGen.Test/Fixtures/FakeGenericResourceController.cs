110bc57a6d61b06cb255572080e2220c9a081289
Javier García de la Noceda Argüelles <jgarcian@riamoneytransfer.com>
2024-07-17 12:26:29 +0200

    PR comments on test of GetUnderlyingGenericTypeMethod

--- test/Swashbuckle.AspNetCore.SwaggerGen.Test/Fixtures/FakeGenericResourceController.cs
+++ test/Swashbuckle.AspNetCore.SwaggerGen.Test/Fixtures/FakeGenericResourceController.cs
@@ -3,22 +3,18 @@ using System.ComponentModel.DataAnnotations;
 using System.Threading;
 using Microsoft.AspNetCore.Mvc;
 
-namespace Swashbuckle.AspNetCore.SwaggerGen.Test.Fixtures
-{
-    internal class FakeGenericResourceController<T>
-        where T : class
-    {
-        public virtual int DifferentMethodsSignatures([FromBody, Required] T t, CancellationToken cancellationToken) => 1;
-        public virtual int DifferentMethodsSignatures([FromBody, Required] string key, T t) => 1;
-        public virtual int DifferentMethodsSignatures([FromBody, Required] T[] ts) => 1;
-        public virtual int DifferentMethodsSignatures([FromBody, Required] string[] strings) => 1;
-        public virtual int DifferentMethodsSignatures([FromBody, Required] List<T> ts) => 1;
-        public virtual int DifferentMethodsSignatures([FromBody, Required] List<string> strings) => 1;
-        public virtual int DifferentMethodsSignatures([FromBody, Required] Dictionary<int, T> ts) => 1;
-        public virtual int DifferentMethodsSignatures([FromBody, Required] IEnumerable<T> ts) => 1;
-    }
-    internal class NonGenericResourceController : FakeGenericResourceController<XmlAnnotatedRecord>
-    {
+namespace Swashbuckle.AspNetCore.SwaggerGen.Test.Fixtures;
 
-    }
+internal class FakeGenericResourceController<T>
+    where T : class
+{
+    public byte DifferentMethodsSignatures([FromBody, Required] T t, CancellationToken cancellationToken) => default;
+    public sbyte DifferentMethodsSignatures([FromBody, Required] string key, T t) => default;
+    public short DifferentMethodsSignatures([FromBody, Required] T[] arrayOfTs) => default;
+    public ushort DifferentMethodsSignatures([FromBody, Required] string[] arrayOfStrings) => default;
+    public int DifferentMethodsSignatures([FromBody, Required] List<T> listOfTs) => default;
+    public uint DifferentMethodsSignatures([FromBody, Required] List<string> listOfStrings) => default;
+    public long DifferentMethodsSignatures([FromBody, Required] Dictionary<int, T> dictionaryOfTs) => default;
+    public ulong DifferentMethodsSignatures([FromBody, Required] IEnumerable<T> iEnumerableOfTs) => default;
 }
+internal class NonGenericResourceController : FakeGenericResourceController<XmlAnnotatedRecord>;
--- test/Swashbuckle.AspNetCore.SwaggerGen.Test/XmlComments/XmlMethodInfoExtensionsTests.cs
+++ test/Swashbuckle.AspNetCore.SwaggerGen.Test/XmlComments/XmlMethodInfoExtensionsTests.cs
@@ -3,21 +3,26 @@ using System.Reflection;
 using Swashbuckle.AspNetCore.SwaggerGen.Test.Fixtures;
 using Xunit;
 
-namespace Swashbuckle.AspNetCore.SwaggerGen.Test
-{
-    public class XmlMethodInfoExtensionsTests
-    {
-        [Theory, MemberData(nameof(DifferentMethodsSignatures))]
-        public void DifferentMethodsSignatures_ShouldNotBeNull(MethodInfo methodInfo)
-        {
-            Assert.NotNull(methodInfo.GetUnderlyingGenericTypeMethod());
-        }
+namespace Swashbuckle.AspNetCore.SwaggerGen.Test;
 
-        public static TheoryData<MethodInfo> DifferentMethodsSignatures =>
-            new(typeof(NonGenericResourceController).GetMethods()
-                .Where(s =>
-                {
-                    return s.Name == nameof(NonGenericResourceController.DifferentMethodsSignatures);
-                }));
+public class XmlMethodInfoExtensionsTests
+{
+    [Theory]
+    [MemberData(nameof(DifferentMethodsSignatures))]
+    public void DifferentMethodsSignatures_ShouldBeExpected(MethodInfo methodInfo)
+    {
+        var underlyingGenericMethod = methodInfo.GetUnderlyingGenericTypeMethod();
+        Assert.NotNull(underlyingGenericMethod);
+        var underlyingGenericMethodParameters = underlyingGenericMethod.GetParameters();
+        Assert.NotEmpty(underlyingGenericMethodParameters);
+        Assert.Equal(methodInfo.GetParameters().Select(s => s.Name), underlyingGenericMethodParameters.Select(s => s.Name));
+        Assert.Equal(methodInfo.ReturnParameter.ParameterType, underlyingGenericMethod.ReturnParameter.ParameterType);
     }
+
+    public static TheoryData<MethodInfo> DifferentMethodsSignatures =>
+        new(typeof(NonGenericResourceController).GetMethods()
+            .Where(s =>
+            {
+                return s.Name == nameof(NonGenericResourceController.DifferentMethodsSignatures);
+            }));
 }
