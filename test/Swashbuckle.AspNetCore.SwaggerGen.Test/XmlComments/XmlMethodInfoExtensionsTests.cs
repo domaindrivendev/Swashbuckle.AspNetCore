1ac66a7739dfe384312ef7b4060b78b021070319
Javier García de la Noceda Argüelles <jgarcian@riamoneytransfer.com>
2024-07-17 13:33:49 +0200

    TheoryData with more information

--- test/Swashbuckle.AspNetCore.SwaggerGen.Test/XmlComments/XmlMethodInfoExtensionsTests.cs
+++ test/Swashbuckle.AspNetCore.SwaggerGen.Test/XmlComments/XmlMethodInfoExtensionsTests.cs
@@ -1,4 +1,6 @@
-﻿using System.Linq;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
 using Swashbuckle.AspNetCore.SwaggerGen.Test.Fixtures;
 using Xunit;
@@ -9,19 +11,20 @@ public class XmlMethodInfoExtensionsTests
 {
     [Theory]
     [MemberData(nameof(DifferentMethodsSignatures))]
-    public void DifferentMethodsSignatures_ShouldBeExpected(MethodInfo methodInfo)
+    public void DifferentMethodsSignatures_ShouldBeExpected((MethodInfo MethodInfo, IEnumerable<string> ExpectedParameterNames, Type ExpectedReturnParameterType) theoryData)
     {
-        var underlyingGenericMethod = methodInfo.GetUnderlyingGenericTypeMethod();
+        var underlyingGenericMethod = theoryData.MethodInfo.GetUnderlyingGenericTypeMethod();
         Assert.NotNull(underlyingGenericMethod);
         var underlyingGenericMethodParameters = underlyingGenericMethod.GetParameters();
         Assert.NotNull(underlyingGenericMethodParameters);
         Assert.NotEmpty(underlyingGenericMethodParameters);
-        Assert.Equal(methodInfo.GetParameters().Select(s => s.Name), underlyingGenericMethodParameters.Select(s => s.Name));
+        Assert.Equal(theoryData.ExpectedParameterNames, underlyingGenericMethodParameters.Select(s => s.Name));
         Assert.NotNull(underlyingGenericMethod.ReturnParameter);
-        Assert.Equal(methodInfo.ReturnParameter.ParameterType, underlyingGenericMethod.ReturnParameter.ParameterType);
+        Assert.Equal(theoryData.ExpectedReturnParameterType, underlyingGenericMethod.ReturnParameter.ParameterType);
     }
 
-    public static TheoryData<MethodInfo> DifferentMethodsSignatures =>
+    public static TheoryData<(MethodInfo MethodInfo, IEnumerable<string> ExpectedParameterNames, Type ExpectedReturnParameterType)> DifferentMethodsSignatures =>
         new(typeof(NonGenericResourceController).GetMethods()
-            .Where(s => s.Name == nameof(NonGenericResourceController.DifferentMethodsSignatures)));
+            .Where(s => s.Name == nameof(NonGenericResourceController.DifferentMethodsSignatures))
+            .Select(s => (s, s.GetParameters().Select(p => p.Name), s.ReturnParameter.ParameterType)));
 }
