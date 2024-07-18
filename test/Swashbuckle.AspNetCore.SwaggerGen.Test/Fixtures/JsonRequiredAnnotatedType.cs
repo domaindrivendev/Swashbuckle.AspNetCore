55d8841fb035806ba5691bc48c1a1cb5bff98ce8
Javier García de la Noceda Argüelles <jgarcian@riamoneytransfer.com>
2024-07-18 17:29:34 +0200

    FileScope nameSpaces

--- test/Swashbuckle.AspNetCore.SwaggerGen.Test/Fixtures/JsonRequiredAnnotatedType.cs
+++ test/Swashbuckle.AspNetCore.SwaggerGen.Test/Fixtures/JsonRequiredAnnotatedType.cs
@@ -1,13 +1,12 @@
 ﻿using System.Text.Json.Serialization;
 
-namespace Swashbuckle.AspNetCore.SwaggerGen.Test.Fixtures
+namespace Swashbuckle.AspNetCore.SwaggerGen.Test.Fixtures;
+
+internal class JsonRequiredAnnotatedType
 {
-    internal class JsonRequiredAnnotatedType
-    {
 
 #if NET7_0_OR_GREATER
-        [JsonRequired]
+    [JsonRequired]
 #endif
-        public string StringWithJsonRequired { get; set; }
-    }
+    public string StringWithJsonRequired { get; set; }
 }
