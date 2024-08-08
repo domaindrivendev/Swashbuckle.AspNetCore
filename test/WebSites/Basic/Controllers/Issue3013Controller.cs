5d2e186bdae222c505173b36495dd542b6cd753f
martincostello <martin@martincostello.com>
2024-08-08 14:09:48 +0100

    Add broken code
    Add example controller code from #3013.

---
+++ test/WebSites/Basic/Controllers/Issue3013Controller.cs
@@ -0,0 +1,26 @@
+﻿#if NET6_0_OR_GREATER
+using Microsoft.AspNetCore.Mvc;
+
+namespace Basic.Controllers;
+
+[ApiController]
+[Route("[controller]/[action]")]
+public class Issue3013Controller : ControllerBase
+{
+    [HttpGet]
+    public TestResponse Get()
+    {
+        return new()
+        {
+            Foo = new(1, 2),
+        };
+    }
+
+    public record TestResponse
+    {
+        public TestStruct? Foo { get; init; }
+    }
+
+    public record struct TestStruct(int A, int B);
+}
+#endif
