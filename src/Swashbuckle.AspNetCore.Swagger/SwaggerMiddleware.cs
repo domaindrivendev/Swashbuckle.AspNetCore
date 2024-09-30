35f5d7cbe18472ae8c10287bf4d26325518023d1
martincostello <martin@martincostello.com>
2024-09-30 10:20:28 +0100

    Make middlewares internal
    Make all the middleware classes internal as they have no useful public-facing functionality (e.g. virtual members).
    
    Resolves #2805.

--- src/Swashbuckle.AspNetCore.Swagger/SwaggerMiddleware.cs
+++ src/Swashbuckle.AspNetCore.Swagger/SwaggerMiddleware.cs
@@ -14,7 +14,7 @@ using Microsoft.OpenApi.Writers;
 
 namespace Swashbuckle.AspNetCore.Swagger
 {
-    public class SwaggerMiddleware
+    internal sealed class SwaggerMiddleware
     {
         private readonly RequestDelegate _next;
         private readonly SwaggerOptions _options;
