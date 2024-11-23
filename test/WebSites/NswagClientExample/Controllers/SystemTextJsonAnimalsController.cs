7d3545f0a0accf14a7f9cd8f6fd7af3d968b26c4
Konstantin Babushkin <koka@idwrx.com>
2024-11-23 12:19:39 +0100

    Support of JsonPolymorphicAttribute Fix integration Basic and NSwagClientExample tests to have different outputs for .net6

---
+++ test/WebSites/NswagClientExample/Controllers/SystemTextJsonAnimalsController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NSwagClientExample.Controllers
+{
+#if NET7_0_OR_GREATER
+    [ApiController]
+    [Route("[controller]")]
+    public class SystemTextJsonAnimalsController : ControllerBase
+    {
+        [HttpPost]
+        public void CreateAnimal([Required]SystemTextJsonAnimal animal)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    [JsonPolymorphic(TypeDiscriminatorPropertyName = "animalType")]
+    [JsonDerivedType(typeof(SystemTextJsonCat), "Cat")]
+    [JsonDerivedType(typeof(SystemTextJsonDog), "Dog")]
+    public class SystemTextJsonAnimal
+    {
+        public string AnimalType { get; set; }
+    }
+
+    public class SystemTextJsonCat : SystemTextJsonAnimal
+    {
+        public string CatSpecificProperty { get; set; }
+    }
+
+    public class SystemTextJsonDog : SystemTextJsonAnimal
+    {
+        public string DogSpecificProperty { get; set; }
+    }
+#endif
+}
