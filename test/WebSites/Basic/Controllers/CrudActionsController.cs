2ee41915b5de249786a129d994515ba96ba90ea7
Evgenii Zakharov <e.zakharov.ext@sportradar.com>
2025-01-25 15:09:48 +0300

    added one more 'para' to justify the implementation

--- test/WebSites/Basic/Controllers/CrudActionsController.cs
+++ test/WebSites/Basic/Controllers/CrudActionsController.cs
@@ -76,7 +76,8 @@ namespace Basic.Controllers
         ///         Only provided properties will be updated (case-sensitive),
         ///         other remain unchanged.
         ///     </para>
-        ///     <para>       Identifier must be non-default value</para>
+        ///     <para>       Identifier must be non-default value     </para>
+        ///     <para>Body must be specified</para>
         /// </remarks>
         /// <param name="id" example="333"></param>
         /// <param name="updates"></param>
