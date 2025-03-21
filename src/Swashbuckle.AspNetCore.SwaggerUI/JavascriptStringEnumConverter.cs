1304289c394bdde8348867606bf90512afc22def
martincostello <martin@martincostello.com>
2025-03-21 16:31:14 +0000

    [SwaggerUI] File-scoped namespaces
    Switch to file-scoped namespaces for `Swashbuckle.AspNetCore.SwaggerUI*`.

--- src/Swashbuckle.AspNetCore.SwaggerUI/JavascriptStringEnumConverter.cs
+++ src/Swashbuckle.AspNetCore.SwaggerUI/JavascriptStringEnumConverter.cs
@@ -1,5 +1,4 @@
-﻿#if NET6_0_OR_GREATER
-using System;
+﻿#if NET
 using System.Diagnostics.CodeAnalysis;
 using System.Text.Json;
 using System.Text.Json.Serialization;
@@ -7,11 +6,7 @@ using System.Text.Json.Serialization;
 namespace Swashbuckle.AspNetCore.SwaggerUI;
 
 internal sealed class JavascriptStringEnumConverter<[DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicFields)] TEnum>() :
-#if NET8_0_OR_GREATER
     JsonStringEnumConverter<TEnum>(JsonNamingPolicy.CamelCase, false)
-#else
-    JsonStringEnumConverter(JsonNamingPolicy.CamelCase, false)
-#endif
     where TEnum : struct, Enum
 {
 }
