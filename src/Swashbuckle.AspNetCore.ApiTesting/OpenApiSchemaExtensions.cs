833b8c5bed18c4283571f54e49c54f28566ac346
martincostello <martin@martincostello.com>
2025-04-03 12:42:07 +0100

    Fix type tests
    Account for flags enum.

--- src/Swashbuckle.AspNetCore.ApiTesting/OpenApiSchemaExtensions.cs
+++ src/Swashbuckle.AspNetCore.ApiTesting/OpenApiSchemaExtensions.cs
@@ -9,47 +9,47 @@ internal static class OpenApiSchemaExtensions
     {
         typedValue = null;
 
-        if (schema.Type == JsonSchemaTypes.Integer && schema.Format == "int64" && long.TryParse(stringValue, out long longValue))
+        if (IsType(schema.Type, JsonSchemaTypes.Integer) && schema.Format == "int64" && long.TryParse(stringValue, out long longValue))
         {
             typedValue = longValue;
         }
-        else if (schema.Type == JsonSchemaTypes.Integer && int.TryParse(stringValue, out int intValue))
+        else if (IsType(schema.Type, JsonSchemaTypes.Integer) && int.TryParse(stringValue, out int intValue))
         {
             typedValue = intValue;
         }
-        else if (schema.Type == JsonSchemaTypes.Number && schema.Format == "double" && double.TryParse(stringValue, out double doubleValue))
+        else if (IsType(schema.Type, JsonSchemaTypes.Number) && schema.Format == "double" && double.TryParse(stringValue, out double doubleValue))
         {
             typedValue = doubleValue;
         }
-        else if (schema.Type == JsonSchemaTypes.Number && float.TryParse(stringValue, out float floatValue))
+        else if (IsType(schema.Type, JsonSchemaTypes.Number) && float.TryParse(stringValue, out float floatValue))
         {
             typedValue = floatValue;
         }
-        else if (schema.Type == JsonSchemaTypes.String && schema.Format == "byte" && byte.TryParse(stringValue, out byte byteValue))
+        else if (IsType(schema.Type, JsonSchemaTypes.String) && schema.Format == "byte" && byte.TryParse(stringValue, out byte byteValue))
         {
             typedValue = byteValue;
         }
-        else if (schema.Type == JsonSchemaTypes.Boolean && bool.TryParse(stringValue, out bool boolValue))
+        else if (IsType(schema.Type, JsonSchemaTypes.Boolean) && bool.TryParse(stringValue, out bool boolValue))
         {
             typedValue = boolValue;
         }
-        else if (schema.Type == JsonSchemaTypes.String && schema.Format == "date" && DateTime.TryParse(stringValue, out DateTime dateValue))
+        else if (IsType(schema.Type, JsonSchemaTypes.String) && schema.Format == "date" && DateTime.TryParse(stringValue, out DateTime dateValue))
         {
             typedValue = dateValue;
         }
-        else if (schema.Type == JsonSchemaTypes.String && schema.Format == "date-time" && DateTime.TryParse(stringValue, out DateTime dateTimeValue))
+        else if (IsType(schema.Type, JsonSchemaTypes.String) && schema.Format == "date-time" && DateTime.TryParse(stringValue, out DateTime dateTimeValue))
         {
             typedValue = dateTimeValue;
         }
-        else if (schema.Type == JsonSchemaTypes.String && schema.Format == "uuid" && Guid.TryParse(stringValue, out Guid uuidValue))
+        else if (IsType(schema.Type, JsonSchemaTypes.String) && schema.Format == "uuid" && Guid.TryParse(stringValue, out Guid uuidValue))
         {
             typedValue = uuidValue;
         }
-        else if (schema.Type == JsonSchemaTypes.String)
+        else if (IsType(schema.Type, JsonSchemaTypes.String))
         {
             typedValue = stringValue;
         }
-        else if (schema.Type == JsonSchemaTypes.Array)
+        else if (IsType(schema.Type, JsonSchemaTypes.Array))
         {
             var arrayValue = schema.Items == null
                 ? stringValue.Split(',')
@@ -67,6 +67,9 @@ internal static class OpenApiSchemaExtensions
         }
 
         return typedValue != null;
+
+        static bool IsType(JsonSchemaType? type, JsonSchemaType target)
+            => type is { } value && value.HasFlag(target);
     }
 
     internal static string TypeIdentifier(this OpenApiSchema schema)
