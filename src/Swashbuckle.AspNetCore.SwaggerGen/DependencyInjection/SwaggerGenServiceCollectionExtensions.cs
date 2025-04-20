ea942d8fdf83120949c0015d9e932e629c6266d1
ItsVeryWindy <ItsVeryWindy@users.noreply.github.com>
2025-04-20 23:29:51 +0100

    fix minimal api json options not being respected

--- src/Swashbuckle.AspNetCore.SwaggerGen/DependencyInjection/SwaggerGenServiceCollectionExtensions.cs
+++ src/Swashbuckle.AspNetCore.SwaggerGen/DependencyInjection/SwaggerGenServiceCollectionExtensions.cs
@@ -44,6 +44,28 @@ public static class SwaggerGenServiceCollectionExtensions
         return services;
     }
 
+    public static IServiceCollection AddSwaggerGenMinimalApisJsonOptions(this IServiceCollection services)
+    {
+        return services.Replace(
+            ServiceDescriptor.Transient<ISerializerDataContractResolver>((s) =>
+            {
+                var options = s.GetRequiredService<IOptionsSnapshot<AspNetCore.Http.Json.JsonOptions>>().Value.SerializerOptions;
+
+                return new JsonSerializerDataContractResolver(options);
+            }));
+    }
+
+    public static IServiceCollection AddSwaggerGenMvcJsonOptions(this IServiceCollection services)
+    {
+        return services.Replace(
+            ServiceDescriptor.Transient<ISerializerDataContractResolver>((s) =>
+            {
+                var options = s.GetRequiredService<IOptionsSnapshot<AspNetCore.Mvc.JsonOptions>>().Value.JsonSerializerOptions;
+
+                return new JsonSerializerDataContractResolver(options);
+            }));
+    }
+
     public static void ConfigureSwaggerGen(
         this IServiceCollection services,
         Action<SwaggerGenOptions> setupAction)
@@ -68,17 +90,34 @@ public static class SwaggerGenServiceCollectionExtensions
             JsonSerializerOptions serializerOptions;
 
             /*
-             * First try to get the options configured for MVC,
-             * then try to get the options configured for Minimal APIs if available,
-             * then try the default JsonSerializerOptions if available,
-             * otherwise create a new instance as a last resort as this is an expensive operation.
+             * There is no surefire way to do this.
+             * However, both JsonOptions are defaulted in the same way.
+             * If neither is configured it makes no difference which one is chosen.
+             * If both are configured, then we just need to make a choice.
+             * As Minimal APIs are newer if someone is configuring them
+             * it's probably more likely that is what they're using.
+             * 
+             * If either JsonOptions is null we will try to create a new instance as
+             * a last resort as this is an expensive operation.
              */
             serializerOptions =
-                _serviceProvider.GetService<IOptions<AspNetCore.Mvc.JsonOptions>>()?.Value?.JsonSerializerOptions
-                ?? _serviceProvider.GetService<IOptions<AspNetCore.Http.Json.JsonOptions>>()?.Value?.SerializerOptions
+                _serviceProvider.GetService<IOptions<AspNetCore.Http.Json.JsonOptions>>()?.Value?.SerializerOptions
                 ?? JsonSerializerOptions.Default;
 
+            if (HasConfiguredMinimalApiJsonOptions())
+            {
+                serializerOptions ??= _serviceProvider.GetService<IOptions<AspNetCore.Http.Json.JsonOptions>>()?.Value?.SerializerOptions;
+            }
+
             return serializerOptions;
+        }
+
+        private bool HasConfiguredMinimalApiJsonOptions()
+        {
+            if (_serviceProvider.GetService<IEnumerable<IConfigureOptions<AspNetCore.Http.Json.JsonOptions>>>().Any())
+                return true;
+
+            return _serviceProvider.GetService<IEnumerable<IPostConfigureOptions<AspNetCore.Http.Json.JsonOptions>>>().Any();
         }
     }
 }
