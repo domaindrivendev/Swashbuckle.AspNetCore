cdd5f6c12425f2ffcf06304249b8c270f96bdfc3
ItsVeryWindy <ItsVeryWindy@users.noreply.github.com>
2025-04-29 23:58:17 +0100

    refactor to use an options class and add extra tests for json options override methods

---
+++ src/Swashbuckle.AspNetCore.SwaggerGen/DependencyInjection/SwaggerGenJsonOptions.cs
@@ -0,0 +1,8 @@
+﻿using System.Text.Json;
+
+namespace Swashbuckle.AspNetCore.SwaggerGen.DependencyInjection;
+
+public class SwaggerGenJsonOptions
+{
+    public JsonSerializerOptions SerializerOptions { get; set; }
+}
