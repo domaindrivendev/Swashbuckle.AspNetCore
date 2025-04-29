d2150a4a64974c543aecadd76acc8febb94c49bc
martincostello <martin@martincostello.com>
2025-04-29 15:38:05 +0100

    Fix build
    Add missing lines from commit.

--- src/Swashbuckle.AspNetCore.Annotations/AnnotationsOperationFilter.cs
+++ src/Swashbuckle.AspNetCore.Annotations/AnnotationsOperationFilter.cs
@@ -117,8 +117,9 @@ public class AnnotationsOperationFilter : IOperationFilter
             operation.Responses[statusCode] = response;
 
             if (response is OpenApiResponse concrete &&
+                swaggerResponseAttribute.ContentTypes is { } contentTypes)
             {
-                response.Content.Clear();
+                concrete.Content?.Clear();
 
                 foreach (var contentType in contentTypes)
                 {
@@ -126,7 +127,7 @@ public class AnnotationsOperationFilter : IOperationFilter
                         ? context.SchemaGenerator.GenerateSchema(swaggerResponseAttribute.Type, context.SchemaRepository)
                         : null;
 
-                    response.Content.Add(contentType, new OpenApiMediaType { Schema = schema });
+                    concrete.Content.Add(contentType, new OpenApiMediaType { Schema = schema });
                 }
             }
         }
