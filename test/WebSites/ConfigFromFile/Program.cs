63c8d3859e999d359f7bb278b981a866f7b0876d
martincostello <martin@martincostello.com>
2025-07-29 16:37:22 +0100

    Remove WebHost usage
    Remove long-deprecated `WebHost` usage and instead use `Host`.

--- test/WebSites/ConfigFromFile/Program.cs
+++ test/WebSites/ConfigFromFile/Program.cs
@@ -1,16 +1,14 @@
-﻿using Microsoft.AspNetCore;
-
-namespace ConfigFromFile;
+﻿namespace ConfigFromFile;
 
 public class Program
 {
-    public static void Main(string[] args)
-    {
-        BuildWebHost(args).Run();
-    }
+    public static void Main(string[] args) =>
+        CreateHostBuilder(args).Build().Run();
 
-    public static IWebHost BuildWebHost(string[] args) =>
-        WebHost.CreateDefaultBuilder(args)
-               .UseStartup<Startup>()
-               .Build();
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseStartup<Startup>();
+            });
 }
