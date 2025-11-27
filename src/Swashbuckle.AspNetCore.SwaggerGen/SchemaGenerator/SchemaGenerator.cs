cdebc52651029f639de0bdfb2e2c55410931a498
martincostello <martin@martincostello.com>
2025-11-27 12:09:58 +0000

    Fix AllOf behaviour when nullable
    When a property is using `AnyOf` and is nullable, convert it to use `OneOf` with null instead.
    
    Resolves #3649.

--- src/Swashbuckle.AspNetCore.SwaggerGen/SchemaGenerator/SchemaGenerator.cs
+++ src/Swashbuckle.AspNetCore.SwaggerGen/SchemaGenerator/SchemaGenerator.cs
@@ -49,12 +49,12 @@ public class SchemaGenerator(
             ? GeneratePolymorphicSchema(schemaRepository, knownTypesDataContracts)
             : GenerateConcreteSchema(dataContract, schemaRepository);
 
+        bool usingAllOf = false;
+
         if (_generatorOptions.UseAllOfToExtendReferenceSchemas && schema is OpenApiSchemaReference reference)
         {
-            schema = new OpenApiSchema()
-            {
-                AllOf = [reference],
-            };
+            schema = new OpenApiSchema() { AllOf = [reference] };
+            usingAllOf = true;
         }
 
         if (schema is OpenApiSchema concrete)
@@ -68,7 +68,22 @@ public class SchemaGenerator(
 
                 var nullable = IsNullable(requiredAttribute, dataProperty, memberInfo);
 
-                SetNullable(concrete, nullable);
+                if (usingAllOf)
+                {
+                    // When using AllOf to extend reference schemas, we need to adjust the schema to represent
+                    // nullability correctly as a property can't be null AND a specific type at the same time.
+                    // See https://github.com/domaindrivendev/Swashbuckle.AspNetCore/issues/3649.
+                    if (nullable)
+                    {
+                        concrete.OneOf = schema.AllOf;
+                        concrete.OneOf.Add(new OpenApiSchema { Type = JsonSchemaType.Null });
+                        concrete.AllOf = null;
+                    }
+                }
+                else
+                {
+                    SetNullable(concrete, nullable);
+                }
 
                 concrete.ReadOnly = dataProperty.IsReadOnly;
                 concrete.WriteOnly = dataProperty.IsWriteOnly;
