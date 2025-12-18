d61f417bb0d3bee85749808c5fcf11df4c660cd6
martincostello <martin@martincostello.com>
2025-12-18 14:50:47 +0000

    Update Microsoft.OpenApi to 2.4.1
    Update to the latest release of Microsoft.OpenApi 2.x.

--- test/Swashbuckle.AspNetCore.IntegrationTests/snapshots/GeneratesValidClient_fed5e45cef80b2b2/Models/Parent.verified.cs
+++ test/Swashbuckle.AspNetCore.IntegrationTests/snapshots/GeneratesValidClient_fed5e45cef80b2b2/Models/Parent.verified.cs
@@ -9,11 +9,9 @@ namespace Swashbuckle.AspNetCore.IntegrationTests.KiotaTests.Models
 {
     [global::System.CodeDom.Compiler.GeneratedCode("Kiota", "1.0.0")]
     #pragma warning disable CS1591
-    public partial class Parent : IAdditionalDataHolder, IParsable
+    public partial class Parent : IParsable
     #pragma warning restore CS1591
     {
-        /// <summary>Stores additional data not described in the OpenAPI description found when deserializing. Can be used for serialization as well.</summary>
-        public IDictionary<string, object> AdditionalData { get; set; }
         /// <summary>The category property</summary>
         public int? Category { get; set; }
         /// <summary>The child property</summary>
@@ -24,13 +22,6 @@ namespace Swashbuckle.AspNetCore.IntegrationTests.KiotaTests.Models
 #else
         public global::Swashbuckle.AspNetCore.IntegrationTests.KiotaTests.Models.Child Child { get; set; }
 #endif
-        /// <summary>
-        /// Instantiates a new <see cref="global::Swashbuckle.AspNetCore.IntegrationTests.KiotaTests.Models.Parent"/> and sets the default values.
-        /// </summary>
-        public Parent()
-        {
-            AdditionalData = new Dictionary<string, object>();
-        }
         /// <summary>
         /// Creates a new instance of the appropriate class based on discriminator value
         /// </summary>
@@ -62,7 +53,6 @@ namespace Swashbuckle.AspNetCore.IntegrationTests.KiotaTests.Models
             if(ReferenceEquals(writer, null)) throw new ArgumentNullException(nameof(writer));
             writer.WriteIntValue("category", Category);
             writer.WriteObjectValue<global::Swashbuckle.AspNetCore.IntegrationTests.KiotaTests.Models.Child>("child", Child);
-            writer.WriteAdditionalData(AdditionalData);
         }
     }
 }
